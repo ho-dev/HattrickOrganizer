9b19d4ce0ccb97d5dd6fce0479a82d878a9da0b8
akasolace <bboum7579@gmail.com>
2018-11-09 21:32:43 +0100

    improve Linux packaging, closes #13

--- buildResources/Linux/ho.sh
+++ buildResources/Linux/ho.sh
@@ -1,30 +1,44 @@
 #!/bin/sh
 
+LAUNCHDIR="$(dirname "$0")"
 HOHOME="~/.ho"
-HODIR=/usr/lib/ho/
-
 HOHOME="`eval echo $HOHOME`"
+LAUNCHDIR="`eval echo $LAUNCHDIR`"
+
+start(){
+
+HODIR=/usr/lib/ho/
 
 if [ ! -d "${HOHOME}" ]
 then
 	echo "creating ${HOHOME}"
 	mkdir -m 777 "${HOHOME}"
-fi
-
-if [ -e "/usr/lib/ho/update.zip" ]
-then
-rm /usr/lib/ho/update.zip
-fi
-
-
-if [ -e "$HOHOME/update.zip" ]
-then
-echo "update found"
-cp -r "$HOHOME/update.zip" "${HODIR}"
-rm "$HOHOME/update.zip"
+	echo "copying required ressource"
+	cp -R "${HODIR}/." "${HOHOME}"
 fi
 
 # Start HOLauncher
 echo "Starting HO from ${HOHOME}...\n"
 cd "${HOHOME}"
-eval "java -cp \"${HODIR}*\" HOLauncher"
+eval "java -cp \"./*\" HOLauncher"
+}
+
+startfromzip(){
+
+# Start HOLauncher
+echo "Starting HO from ${LAUNCHDIR}...\n"
+cd "${LAUNCHDIR}"
+eval "java -cp \"./*\" HOLauncher"
+}
+
+if [ "/usr/bin" = "${LAUNCHDIR}" ]
+then
+   echo "started from installed application"
+   start
+else
+   echo "started from zip"
+   startfromzip
+fi
+
+
+exit 0
