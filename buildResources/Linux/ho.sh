587b784a205b6f52693a40b57cac82705e0f4838
yaute <yaute74@gmail.com>
2018-12-03 21:11:13 +0100

    copy db from old user directory to new directory on Linux

--- buildResources/Linux/ho.sh
+++ buildResources/Linux/ho.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
 
 LAUNCHDIR="$(dirname "$0")"
-HOHOME="~/.ho"
-HOHOME="`eval echo $HOHOME`"
 LAUNCHDIR="`eval echo $LAUNCHDIR`"
+HOHOME="~/.local/share/ho"
+HOHOME="`eval echo $HOHOME`"
+OLDHOHOME="~/.hattrickorganizer"
+OLDHOHOME="`eval echo $OLDHOHOME`"
 
 start(){
+    HODIR=/usr/lib/ho/
 
-HODIR=/usr/lib/ho/
-
-if [ ! -d "${HOHOME}" ]
-then
-	echo "creating ${HOHOME}"
-	mkdir -m 777 "${HOHOME}"
-	echo "copying required ressource"
-	cp -R "${HODIR}/." "${HOHOME}"
-fi
-
-# Start HOLauncher
-echo "Starting HO from ${HOHOME}...\n"
-cd "${HOHOME}"
-eval "java -cp \"./*\" HOLauncher"
+    if [ ! -d "${HOHOME}" ]
+    then
+        echo "creating ${HOHOME}"
+        mkdir -m 755 "${HOHOME}"
+        echo "copying required ressource"
+        cp -R "${HODIR}/." "${HOHOME}"
+        
+        # Copy db from old version
+        if [ -d ${OLDHOHOME}/db ]
+        then
+            cp -r ${OLDHOHOME}/db ${HOHOME}/db
+            BKPDATE=`date +%Y-%m-%d_%H-%M-%S`
+            mv ${OLDHOHOME} ${HOHOME}/backup_${BKPDATE}
+        fi
+    fi
+    
+    # Start HOLauncher
+    echo "Starting HO from ${HOHOME}...\n"
+    cd "${HOHOME}"
+    eval "java -cp \"./*\" HOLauncher"
 }
 
 startfromzip(){
-
-# Start HOLauncher
-echo "Starting HO from ${LAUNCHDIR}...\n"
-cd "${LAUNCHDIR}"
-eval "java -cp \"./*\" HOLauncher"
+    # Start HOLauncher
+    echo "Starting HO from ${LAUNCHDIR}...\n"
+    cd "${LAUNCHDIR}"
+    eval "java -cp \"./*\" HOLauncher"
 }
 
 if [ "/usr/bin" = "${LAUNCHDIR}" ]
@@ -39,6 +47,5 @@ else
    echo "started from zip"
    startfromzip
 fi
-
 
 exit 0
