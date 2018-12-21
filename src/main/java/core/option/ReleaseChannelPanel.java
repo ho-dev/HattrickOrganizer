f701e22cbd16ad516d319b60f348528dddac6a8f
akasolace <bboum7579@gmail.com>
2018-12-21 18:26:43 +0100

    clean up of licence files added some description to release channels initiate work on making HO release from gradle

--- src/main/java/core/option/ReleaseChannelPanel.java
+++ src/main/java/core/option/ReleaseChannelPanel.java
@@ -1,15 +1,11 @@
 package core.option;
 
-
-
 import core.gui.comp.panel.ImagePanel;
 
 import java.awt.GridLayout;
 import java.awt.Font;
 
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
-import javax.swing.JLabel;
+import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 
 
@@ -25,6 +21,7 @@ public final class ReleaseChannelPanel extends ImagePanel
     JRadioButton jrb_Stable;
     JRadioButton jrb_Beta;
     JRadioButton jrb_Dev;
+    JTextArea  jl_Description;
 
 
     /**
@@ -39,11 +36,14 @@ public final class ReleaseChannelPanel extends ImagePanel
     public final void itemStateChanged(java.awt.event.ItemEvent itemEvent) {
         // TODO: manage saving into DB
         if (jrb_Stable.isSelected()){
-        core.model.UserParameter.temp().ReleaseChannel = "Stable";}
+        core.model.UserParameter.temp().ReleaseChannel = "Stable";
+            jl_Description.setText(core.model.HOVerwaltung.instance().getLanguageString("options.release_channels_STABLE_desc"));}
         else if (jrb_Beta.isSelected()){
-            core.model.UserParameter.temp().ReleaseChannel = "Beta";}
+            core.model.UserParameter.temp().ReleaseChannel = "Beta";
+            jl_Description.setText(core.model.HOVerwaltung.instance().getLanguageString("options.release_channels_BETA_desc"));}
         else{
-            core.model.UserParameter.temp().ReleaseChannel = "Dev";}
+            core.model.UserParameter.temp().ReleaseChannel = "Dev";
+            jl_Description.setText(core.model.HOVerwaltung.instance().getLanguageString("options.release_channels_DEV_desc"));}
     }
 
     public void stateChanged(ChangeEvent arg0) {
@@ -51,7 +51,7 @@ public final class ReleaseChannelPanel extends ImagePanel
     }
 
     private void initComponents() {
-        setLayout(new GridLayout(10, 1, 4, 4));
+        setLayout(new GridLayout(6, 1, 0, 0));
 
         JLabel jlQuestion = new JLabel(core.model.HOVerwaltung.instance().getLanguageString("options.release_channels_pleaseSelect"));
         Font f = jlQuestion.getFont();
@@ -67,16 +67,23 @@ public final class ReleaseChannelPanel extends ImagePanel
         jbg_buttonGroup.add(jrb_Beta);
         jbg_buttonGroup.add(jrb_Dev);
 
+        jl_Description = new JTextArea("", 5, 1);
+        jl_Description.setEditable(false);
 
-        String sReleaseChannel = core.model.UserParameter.temp().ReleaseChannel;
+
 
         switch (core.model.UserParameter.temp().ReleaseChannel) {
             case "Beta":
                 jrb_Beta.setSelected(true);
+                jl_Description.setText(core.model.HOVerwaltung.instance().getLanguageString("options.release_channels_BETA_desc"));
                 break;
             case "Dev":
                 jrb_Dev.setSelected(true);
+                jl_Description.setText(core.model.HOVerwaltung.instance().getLanguageString("options.release_channels_DEV_desc"));
                 break;
+            default:
+                jl_Description.setText(core.model.HOVerwaltung.instance().getLanguageString("options.release_channels_STABLE_desc"));
+
         }
 
         //Register a listener for the radio buttons.
@@ -87,6 +94,10 @@ public final class ReleaseChannelPanel extends ImagePanel
         add(jrb_Stable);
         add(jrb_Beta);
         add(jrb_Dev);
+
+//        add(new JSeparator());
+
+        add(jl_Description);
 
 
     }
