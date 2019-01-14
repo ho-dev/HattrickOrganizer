182a83720376ad8fae87ab79eb505eb2a5682689
Aleksander Lorenc <zedlord89@gmail.com>
2019-01-14 18:08:19 +0100

    moved (and disabled) option UpdateCheck
    from check tab to release channel tab

--- src/main/java/core/option/CheckOptionPanel.java
+++ src/main/java/core/option/CheckOptionPanel.java
@@ -20,7 +20,6 @@ public final class CheckOptionPanel extends ImagePanel
     //~ Static fields/initializers -----------------------------------------------------------------
 
 	private static final long serialVersionUID = 1L;
-    private JCheckBox m_jchUpdateCheck;
 
     //~ Constructors -------------------------------------------------------------------------------
 
@@ -34,23 +33,12 @@ public final class CheckOptionPanel extends ImagePanel
     //~ Methods ------------------------------------------------------------------------------------
 
 
-    public final void itemStateChanged(java.awt.event.ItemEvent itemEvent) {        
-        core.model.UserParameter.temp().updateCheck = m_jchUpdateCheck.isSelected();
-    }
+    public final void itemStateChanged(java.awt.event.ItemEvent itemEvent) {}
 
-	public void stateChanged(ChangeEvent arg0) {
-				
-	}
+	public void stateChanged(ChangeEvent arg0) {}
 
     private void initComponents() {
         setLayout(new GridLayout(10, 1, 4, 4));
-
-        m_jchUpdateCheck = new JCheckBox(core.model.HOVerwaltung.instance().getLanguageString("UpdateCheck"));
-        m_jchUpdateCheck.setToolTipText(core.model.HOVerwaltung.instance().getLanguageString("tt_Optionen_UpdateCheck"));
-        m_jchUpdateCheck.setOpaque(false);
-        m_jchUpdateCheck.setSelected(core.model.UserParameter.temp().updateCheck);
-        m_jchUpdateCheck.addItemListener(this);
-        add(m_jchUpdateCheck);
 
         for(int i = 0; i < 6; i++) {
         	add(new JLabel(""));
