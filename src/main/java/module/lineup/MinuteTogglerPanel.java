272ff0a6c7c909938bf73942306b59f7f086d242
Aleksander Lorenc <zedlord89@gmail.com>
2019-06-02 11:07:15 +0200

    wired up shift buttons (part one)

--- src/main/java/module/lineup/MinuteTogglerPanel.java
+++ src/main/java/module/lineup/MinuteTogglerPanel.java
@@ -5,7 +5,9 @@ import core.model.HOVerwaltung;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ListIterator;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
@@ -18,6 +20,7 @@ public final class MinuteTogglerPanel extends JPanel {
 	
 	private List<JLabel> toggleKeys = new ArrayList();
 	private List<JLabel> toggleKeysET = new ArrayList();
+	private int current = 0;
 	
 	public MinuteTogglerPanel() {
 
@@ -26,13 +29,22 @@ public final class MinuteTogglerPanel extends JPanel {
 
 	private void initComponents() {
 		setLayout(new GridLayout(1,1));
-		add(new JLabel("<<", SwingConstants.CENTER));
+		JLabel prevButton = new JLabel("<<", SwingConstants.CENTER);
+		prevButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				shiftBackward(1);
+				revalidate();
+			}
+		});
+		add(prevButton);
 		List<Double> toggleLabels = new ArrayList(HOVerwaltung.instance().getModel().getLineup().getRatings().getLeftDefense().keySet());
 		Collections.sort(toggleLabels);
 		for(int i=0;i<toggleLabels.size();i++) {
 			JLabel toggleLabel = new JLabel(String.valueOf(toggleLabels.get(i).longValue()), SwingConstants.CENTER);
-			if(i%2 == 0) toggleLabel.setBackground(java.awt.Color.GREEN);
-			else toggleLabel.setBackground(java.awt.Color.RED);
+			toggleLabel.setForeground(Color.BLACK);
+			if(i%2 == 0) toggleLabel.setBackground(Color.GREEN);
+			else toggleLabel.setBackground(Color.RED);
 			toggleLabel.setOpaque(true);
 			if(toggleLabels.get(i) > 90d) {
 				toggleKeysET.add(toggleLabel);
@@ -41,7 +53,17 @@ public final class MinuteTogglerPanel extends JPanel {
 				toggleKeys.add(toggleLabel);
 			}
 		}
-		add(new JLabel(">>", SwingConstants.CENTER));
+		toggleKeys.get(0).setForeground(Color.GREEN);
+		toggleKeys.get(0).setBackground(Color.BLACK);
+		JLabel nextButton = new JLabel(">>", SwingConstants.CENTER);
+		nextButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				shiftForward(1);
+				revalidate();
+			}
+		});
+		add(nextButton);
 		JLabel ETButton = new JLabel("ET", SwingConstants.CENTER); //toggle extra time visibility (on default: not visible)
 		ETButton.addMouseListener(new MouseAdapter() {
 			boolean enabled = false;
@@ -64,5 +86,47 @@ public final class MinuteTogglerPanel extends JPanel {
 			}
 		});
 		add(ETButton);
+	}
+
+	private void shiftForward(int value) {
+		ListIterator<JLabel> keyIterator = toggleKeys.listIterator(current);
+		JLabel key = keyIterator.next();
+		reverseColor(key);
+		while(value != 0) {
+			if(keyIterator.hasNext()) key = keyIterator.next();
+			else {
+				keyIterator = toggleKeys.listIterator();
+				key = keyIterator.next();
+			  }
+			value--;
+		}
+		if(keyIterator.hasPrevious()) current = keyIterator.previousIndex();
+		else current = toggleKeys.size() - 1;
+		reverseColor(key);
+	}
+
+	private void shiftBackward(int value) {
+		ListIterator<JLabel> keyIterator = toggleKeys.listIterator(current + 1);
+		JLabel key = keyIterator.previous();
+		reverseColor(key);
+		while(value != 0) {
+			if(keyIterator.hasPrevious()) {
+				key = keyIterator.previous();
+			} else {
+				keyIterator = toggleKeys.listIterator(toggleKeys.size());
+				key = keyIterator.previous();
+			  }
+			value--;
+		}
+		if(keyIterator.hasNext()) current = keyIterator.nextIndex();
+		else current = 0;
+		reverseColor(key);
+	}
+
+	private void reverseColor(JLabel target) {
+		Color fg = target.getForeground();
+        Color bg = target.getBackground();
+		target.setForeground(bg);
+		target.setBackground(fg);
 	}
 }
