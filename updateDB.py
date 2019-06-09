6a6f8fd74a4fec38cbd56463a12020eef793cc8e
akasolace <thisisakasolace@gmail.com>
2019-06-09 18:30:01 +0200

    Delete updateDB.py

--- updateDB.py
+++
@@ -1,9 +0,0 @@
-from shutil import copytree, rmtree
-import os
-
-if os.path.exists(r"D:\TEMP\db"):
-    rmtree(r"D:\TEMP\db")
-copytree(r"C:\Users\Sabry CHEIKHROUHOU\Dropbox\Perso\HattrickOrganizer\db", r"D:\TEMP\db")
-if os.path.exists(r"D:\Perso\Code\HO\db"):
-    rmtree(r"D:\Perso\Code\HO\db")
-copytree(r"C:\Users\Sabry CHEIKHROUHOU\Dropbox\Perso\HattrickOrganizer\db", r"D:\Perso\Code\HO\db")
