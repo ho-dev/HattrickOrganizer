811652c1c0551cda45355a68c9640e374986e0b6
pierpaolocira <pierpaolocira@users.noreply.github.com>
2019-08-17 21:02:00 +0200

    FileLoader applied to RatingPredictionParameter(2)

--- src/main/java/core/rating/RatingPredictionParameter.java
+++ src/main/java/core/rating/RatingPredictionParameter.java
@@ -5,8 +5,6 @@ import core.util.HOLogger;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Enumeration;
