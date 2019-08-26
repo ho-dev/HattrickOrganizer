262309879b6d60215a6067cd4f95772fa14f62dd
Aleksander Lorenc <zedlord89@gmail.com>
2019-08-26 21:01:38 +0200

    added controls to rating chart window

--- src/main/java/module/lineup/RatingChartFrame.java
+++ src/main/java/module/lineup/RatingChartFrame.java
@@ -3,18 +3,41 @@ package module.lineup;
 import core.model.HOVerwaltung;
 import core.gui.HOMainFrame;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JCheckBox;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
 
 class RatingChartFrame extends JFrame {
+
+	private JRadioButton singleChartButton = new JRadioButton(HOVerwaltung.instance().getLanguageString("lineup.SingleChart"));
+	private JRadioButton multiChartButton = new JRadioButton(HOVerwaltung.instance().getLanguageString("lineup.MultiChart"));
+	private ButtonGroup chartButtonGroup = new ButtonGroup();
+	private JCheckBox etToggler = new JCheckBox(HOVerwaltung.instance().getLanguageString("lineup.ETToggler"));
+	private JPanel controlsPanel = new JPanel();
 
 	RatingChartFrame() {
 		super(HOVerwaltung.instance().getLanguageString("RatingChartFrame"));
 		this.setIconImage(HOMainFrame.instance().getIconImage());
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLayout(new BorderLayout());
 		initComponents();
 	}
 
 	private void initComponents() {
+	singleChartButton.setEnabled(false);
+	multiChartButton.setEnabled(false);
+	etToggler.setEnabled(false);
+	chartButtonGroup.add(singleChartButton);
+	chartButtonGroup.add(multiChartButton);
+	controlsPanel.add(singleChartButton);
+	controlsPanel.add(multiChartButton);
+	controlsPanel.add(etToggler);
+	add(controlsPanel, BorderLayout.NORTH);
+	pack();
 	setVisible(true);
 	}
 }
