f184d656e29270a67052008fe44cdc660ced5152
Aleksander Lorenc <zedlord89@gmail.com>
2019-08-31 14:45:46 +0200

    added single chart subclass
    for multi chart rating panel

--- src/main/java/module/lineup/MultipleRatingChartsPanel.java
+++ src/main/java/module/lineup/MultipleRatingChartsPanel.java
@@ -1,6 +1,11 @@
 package module.lineup;
 
 import core.model.HOVerwaltung;
+import core.util.Helper;
+import core.gui.model.StatistikModel;
+import module.statistics.StatistikPanel;
+
+import java.text.NumberFormat;
 
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
@@ -9,6 +14,31 @@ import javax.swing.JPanel;
 import javax.swing.JCheckBox;
 
 public final class MultipleRatingChartsPanel extends JPanel {
+
+	private final class SingleChart {
+
+		private StatistikPanel chart = new StatistikPanel(true);
+		private StatistikModel[] data = new StatistikModel[1];
+		private String[] captions;
+		private double[] values;
+		private NumberFormat format;
+
+		public SingleChart(double[] values, NumberFormat format, String[] captions) {
+			this.values = values;
+			this.format = format;
+			this.captions = captions;
+			initComponents();
+		}
+
+		private void initComponents() {
+			data[0] = new StatistikModel(values, null, true, java.awt.Color.black, format);
+			chart.setAllValues(data, captions, format, "", "", false, true);
+		}
+
+		public StatistikPanel getChart() {
+			return chart;
+		}
+	}
 
 	private JPanel controlsPanel = new JPanel();
 	private JPanel chartsPanel = new JPanel(new GridBagLayout());
