5691d240e97eff79abf361def5769e6603da8e1b
Aleksander Lorenc <zedlord89@gmail.com>
2019-09-08 02:34:02 +0200

    added controls panel to MultipleRatingChartsPanel

--- src/main/java/module/lineup/MultipleRatingChartsPanel.java
+++ src/main/java/module/lineup/MultipleRatingChartsPanel.java
@@ -1,13 +1,28 @@
 package module.lineup;
 
+import core.model.HOVerwaltung;
+
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
+import javax.swing.JCheckBox;
 
 public final class MultipleRatingChartsPanel extends JPanel {
 
+	private JPanel controlsPanel = new JPanel();
+	private JCheckBox showHelpLines = new JCheckBox(HOVerwaltung.instance().getLanguageString("Hilflinien"));
+	private JCheckBox showValues = new JCheckBox(HOVerwaltung.instance().getLanguageString("Beschriftung"));
+
 	public MultipleRatingChartsPanel() {
+		super(new BorderLayout());
 		initComponents();
 	}
 
 	private void initComponents() {
+		showHelpLines.setEnabled(false);
+		showValues.setEnabled(false);
+		controlsPanel.add(showHelpLines);
+		controlsPanel.add(showValues);
+		add(controlsPanel, BorderLayout.SOUTH);
 	}
 }
