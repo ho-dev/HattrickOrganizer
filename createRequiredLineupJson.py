dffefd6d691bc0ed7d6b6c1c50f93db64ca5cc13
akasolace <thisiakasolace@gmail.com>
2019-10-03 20:49:33 +0200

    new requiredLineup

--- createRequiredLineupJson.py
+++ createRequiredLineupJson.py
@@ -42,6 +42,15 @@ class Tactic(Enum):
     PC = "playcreatively"
     LS = "longshots"
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def validateLineup(requiredLineup):
     for position, order, bPresent in requiredLineup:
@@ -69,7 +78,21 @@ def validateLineup(requiredLineup):
                 raise ValueError(f"position {position} is not yet handled")
 
 
-def createJson(lineupName, requiredLineup, attitude, tactic):
+def validate_answer(_answer):
+    try:
+        _answer = _answer.lower()
+    except:
+        print("Answer was not understood")
+        return None
+    if (_answer == 'y') or (_answer == "yes"):
+        return "yes"
+    elif (_answer == 'no') or (_answer == 'no'):
+        return "no"
+    else:
+        print("Answer was not understood")
+        return None
+
+def createJson(lineupName, requiredLineup, attitude, tactic, bPRODUCTION):
     attitude, tactic = attitude.value, tactic.value
     validateLineup(requiredLineup)
     json_data = {}
@@ -83,10 +106,24 @@ def createJson(lineupName, requiredLineup, attitude, tactic):
     json_data["attitude"] = attitude
     json_data["tatic"] = tactic
 
-    with open(r"D:\TEMP\feedback.json", "w") as write_file:
-        json.dump(json_data, write_file, indent=4)
-    # with open(r"docs/feedback.json", "w") as write_file:
-    #     json.dump(json_data, write_file, indent=4)
+    path = r"D:\TEMP\feedback.json"
+    answer = None
+
+    if not bPRODUCTION:
+        while (answer != "yes" and answer != "no"):
+            answer = input("Pushing to " + bcolors.OKGREEN + "TEST" + bcolors.ENDC + ", do you want to continue, (y)es / (n)o ? ")
+            answer = validate_answer(answer)
+    else:
+        while (answer != "yes" and answer != "no"):
+            answer = input("Pushing to " + bcolors.WARNING + "PRODUCTION" + bcolors.ENDC + ", do you want to continue, (y)es / (n)o ?  ")
+            answer = validate_answer(answer)
+
+    if answer == "yes":
+        if bPRODUCTION:
+            path = r"docs/feedback.json"
+        with open(path, "w") as write_file:
+            json.dump(json_data, write_file, indent=4)
+
 
 
 requiredLineup = []
@@ -97,4 +134,4 @@ requiredLineup.append((Position.CD, MatchOrder.NORMAL, True))
 # requiredLineup.append((Position.WIl, MatchOrder.TOWARDS_MIDDLE, False))
 # requiredLineup.append((Position.FWl, MatchOrder.TOWARDS_WING, False))
 
-createJson("CDc-Normal", requiredLineup, Attitute.NORMAL, Tactic.NORMAL)
+createJson("CDc-Normal", requiredLineup, Attitute.NORMAL, Tactic.NORMAL, bPRODUCTION = True)
