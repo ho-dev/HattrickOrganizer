4ae8995f7dffa4b153f72967e8c1e911a4ec6869
MisterMax80 <giglimassimo@gmail.com>
2019-12-18 08:41:58 +0100

    Feature for #213 best positions (#322)
    * feature for #213
    - player overview
    - preference
    
    * 2.0 language quick fixes
    
    * start dvlpt cycle of 3.0
    
    * feature for #213
    - player overview
    - preference
    
    * #125 traning preview for stamina (#298)
    
    * Update README.md
    
    * Update README.md
    
    * feature for #213
    - player details
    
    * feature for #213
    - player details
    
    * feature for #213
    - player details simulator
    
    * feature for #213
    - lineup tab
    
    * feature for #213
    - lineup work fine
    
    * feature for #213
    - adjust user preference
    - * in player overview ideal position

--- src/main/java/core/gui/model/PlayerPositionColumn.java
+++ src/main/java/core/gui/model/PlayerPositionColumn.java
@@ -16,89 +16,105 @@ import javax.swing.table.TableColumn;
 
 /**
  * Column shows a position skill of a player
+ *
  * @author Thorsten Dietz
  * @since 1.36
  */
 class PlayerPositionColumn extends PlayerColumn {
 
-	/** position id **/
-	private byte position;
+    /**
+     * position id
+     **/
+    private byte position;
 
-     /**
+    /**
      * constructor
+     *
      * @param id
      * @param name
      * @param tooltip
      * @param position
      */
-	protected PlayerPositionColumn(int id,String name, String tooltip,byte position){
-		super(id,name,tooltip);
-		this.position = position;
-	}
-	
-	/**
-	 * returns TableEntry
-	 * will not be overwrite
-	 */
-	@Override
-	public IHOTableEntry getTableEntry(Player player, Player comparePlayer){
-		return new DoppelLabelEntry(getEntryValue(player),getCompareValue(player,comparePlayer));
-	}
-	
-	/**
-	 * returns player value
-	 * overwritten by created columns
-	 * @param player
-	 * @return
-	 */
-	public ColorLabelEntry getEntryValue(Player player){
-		return new ColorLabelEntry(player.calcPosValue(position, true),
-				getBackgroundColor(), false,
-				core.model.UserParameter.instance().nbDecimals);
-	}
-	
-	/**
-	 * return a value if comparePlayer is not null
-	 * @param player
-	 * @param comparePlayer
-	 * @return ColorLabelEntry
-	 */
-	public ColorLabelEntry getCompareValue(Player player, Player comparePlayer){
-		if(comparePlayer == null){
-			return new ColorLabelEntry("",
-		            ColorLabelEntry.FG_STANDARD,
-		            getBackgroundColor(),
-		            SwingConstants.RIGHT);
-		}
-		
-		return new ColorLabelEntry(player.calcPosValue(position, true)
-				-comparePlayer.calcPosValue(position, true),
-				getBackgroundColor(),false,false,
-				core.model.UserParameter.instance().nbDecimals);
-		
-	}
-	
-	/**
-	 * overwrite the method from UserColumn
-	 */
-	@Override
-	public void setSize(TableColumn column){
-		final int breite = (int) (55d * (1d + ((core.model.UserParameter.instance().nbDecimals - 1) / 4.5d)));
-		column.setMinWidth(25);
-		column.setPreferredWidth((preferredWidth == 0)?Helper.calcCellWidth(breite):preferredWidth);
-	}
-	
-	private Color getBackgroundColor(){
-		switch(position){
-		case IMatchRoleID.KEEPER:
-		case IMatchRoleID.CENTRAL_DEFENDER:
-		case IMatchRoleID.BACK:
-		case IMatchRoleID.MIDFIELDER:
-		case IMatchRoleID.WINGER:
-		case IMatchRoleID.FORWARD:
-			return ColorLabelEntry.BG_PLAYERSPOSITIONVALUES;
-		default:
-			return ColorLabelEntry.BG_PLAYERSSUBPOSITIONVALUES;
-		}
-	}
+    protected PlayerPositionColumn(int id, String name, String tooltip, byte position) {
+        super(id, name, tooltip);
+        this.position = position;
+    }
+
+    /**
+     * returns TableEntry
+     * will not be overwrite
+     */
+    @Override
+    public IHOTableEntry getTableEntry(Player player, Player comparePlayer) {
+        return new DoppelLabelEntry(getEntryValue(player), getCompareValue(player, comparePlayer));
+    }
+
+    /**
+     * returns player value
+     * overwritten by created columns
+     *
+     * @param player
+     * @return
+     */
+    public ColorLabelEntry getEntryValue(Player player) {
+        ColorLabelEntry temp = new ColorLabelEntry(player.calcPosValue(position, true),
+                getBackgroundColor(), false,
+                core.model.UserParameter.instance().nbDecimals);
+
+        byte[] alternativePosition = player.getAlternativePositions();
+        for (byte altPos : alternativePosition) {
+            if (altPos == position) {
+                temp.setBold(true);
+                break;
+            }
+        }
+
+        return temp;
+    }
+
+    /**
+     * return a value if comparePlayer is not null
+     *
+     * @param player
+     * @param comparePlayer
+     * @return ColorLabelEntry
+     */
+    public ColorLabelEntry getCompareValue(Player player, Player comparePlayer) {
+        if (comparePlayer == null) {
+            return new ColorLabelEntry("",
+                    ColorLabelEntry.FG_STANDARD,
+                    getBackgroundColor(),
+                    SwingConstants.RIGHT);
+        }
+
+        return new ColorLabelEntry(player.calcPosValue(position, true)
+                - comparePlayer.calcPosValue(position, true),
+                getBackgroundColor(), false, false,
+                core.model.UserParameter.instance().nbDecimals);
+
+    }
+
+    /**
+     * overwrite the method from UserColumn
+     */
+    @Override
+    public void setSize(TableColumn column) {
+        final int breite = (int) (55d * (1d + ((core.model.UserParameter.instance().nbDecimals - 1) / 4.5d)));
+        column.setMinWidth(25);
+        column.setPreferredWidth((preferredWidth == 0) ? Helper.calcCellWidth(breite) : preferredWidth);
+    }
+
+    private Color getBackgroundColor() {
+        switch (position) {
+            case IMatchRoleID.KEEPER:
+            case IMatchRoleID.CENTRAL_DEFENDER:
+            case IMatchRoleID.BACK:
+            case IMatchRoleID.MIDFIELDER:
+            case IMatchRoleID.WINGER:
+            case IMatchRoleID.FORWARD:
+                return ColorLabelEntry.BG_PLAYERSPOSITIONVALUES;
+            default:
+                return ColorLabelEntry.BG_PLAYERSSUBPOSITIONVALUES;
+        }
+    }
 }
