29cb50d79d231e1d5f542c82b87521f27b2a3377
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2020-03-21 10:17:05 +0100

    npe candidates
    (cherry picked from commit fda1eec61636f733daee9bd7cf1cfe9da0755c62)

--- src/main/java/core/specialevents/UnpredictableEventPredictionAnalyzer.java
+++ src/main/java/core/specialevents/UnpredictableEventPredictionAnalyzer.java
@@ -56,6 +56,7 @@ public class UnpredictableEventPredictionAnalyzer implements  ISpecialEventPredi
         this.analyse = analyse;
 
         int id = position.getSpielerId();
+        if ( id == 0 ) return;
         Player p = analyse.getPlayer(id);
         if (p.hasSpeciality(Speciality.UNPREDICTABLE)) {
             switch (position.getId()) {
@@ -118,10 +119,11 @@ public class UnpredictableEventPredictionAnalyzer implements  ISpecialEventPredi
     }
 
     private void getUnpredictableOwnGoal( MatchRoleID position) {
+        if (position.getSpielerId() == 0) return;
         Player p = this.analyse.getPlayer(position.getSpielerId());
         Player keeper = this.analyse.getPlayerByPosition(IMatchRoleID.keeper);
-        double gkSkill=0;
-        if (keeper != null){
+        double gkSkill = 0;
+        if (keeper != null) {
             gkSkill = keeper.getGKskill();
         }
 
@@ -131,13 +133,14 @@ public class UnpredictableEventPredictionAnalyzer implements  ISpecialEventPredi
                 20 - p.getPSskill() - gkSkill
         );
 
-        if ( se != null){
+        if (se != null) {
             analyse.addSpecialEventPrediction(se);
         }
     }
 
     private void getUnpredictableSpecialAction( MatchRoleID position, int leftCentralDefender, int middleCentralDefender, int rightCentralDefender) {
         // Calculate opponent Defence skill
+        if (position.getSpielerId() == 0) return;
         Player p = this.analyse.getPlayer(position.getSpielerId());
         double opponentDefenceSkill = 0;
         double opponentExperience = 0;
@@ -170,7 +173,7 @@ public class UnpredictableEventPredictionAnalyzer implements  ISpecialEventPredi
         SpecialEventsPrediction se = SpecialEventsPrediction.createIfInRange(position, SpecialEventType.UNPREDICTABLE,
                 .5, 10, -10,
                 p.getPSskill() + p.getErfahrung() - opponentDefenceSkill - opponentExperience);
-        if ( se != null){
+        if (se != null) {
             getPassReceivers(se);
         }
     }
@@ -195,7 +198,7 @@ public class UnpredictableEventPredictionAnalyzer implements  ISpecialEventPredi
     private void getUnpredictableMistake( MatchRoleID position, int opponentPos, double wingerScoreBonus) {
         Player p = this.analyse.getPlayer(position.getSpielerId());
         Player op = this.analyse.getOpponentPlayerByPosition(opponentPos);
-        if ( op != null){
+        if ( op != null && p != null){
             SpecialEventsPrediction se = SpecialEventsPrediction.createIfInRange(position,
                     SpecialEventType.UNPREDICTABLE_MISTAKE,
                     -.5,10, -10,
@@ -213,6 +216,7 @@ public class UnpredictableEventPredictionAnalyzer implements  ISpecialEventPredi
     }
     
     private void getUnpredictableLongPass( MatchRoleID position) {
+        if (position.getSpielerId() == 0) return;
         Player p = this.analyse.getPlayer(position.getSpielerId());
         for (int i = IMatchRoleID.rightInnerMidfield; i <= IMatchRoleID.leftInnerMidfield; i++) {
             Player opp = this.analyse.getOpponentPlayerByPosition(i);
