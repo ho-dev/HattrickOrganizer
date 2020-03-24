c87b818e0dd6b3261ae989914afb2dc4f8d44631
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2020-03-24 21:57:45 +0100

    fix bug with supporter specialty

--- src/main/java/core/constants/player/Speciality.java
+++ src/main/java/core/constants/player/Speciality.java
@@ -1,7 +1,7 @@
 package core.constants.player;
 	// enum for speciality
 	public enum Speciality {
-		NO_SPECIALITY(0), TECHNICAL(1), QUICK(2), POWERFUL(3), UNPREDICTABLE(4), HEAD(5), REGAINER(6), SUPPORT(8);
+		NO_SPECIALITY(0), TECHNICAL(1), QUICK(2), POWERFUL(3), UNPREDICTABLE(4), HEAD(5), REGAINER(6), _UNUSED(7), SUPPORT(8);
 		private final int value;
 
 		Speciality(int spec) {
