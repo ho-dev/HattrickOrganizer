fc668ed917fbd4671363f448c062f180634d441e
akasolace <thisiakasolace@gmail.com>
2020-05-29 14:02:30 +0200

    setup toolcahin with install4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 open module ho {
     requires java.desktop;
     requires java.sql;
-    requires okhttp3;
+//    requires okhttp3;
     requires scribe;
     requires com.google.gson;
     requires com.google.common;
