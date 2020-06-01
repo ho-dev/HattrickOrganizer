909e427ab0f5170fcd7cfe766e1fcc8e88aa3336
Sébastien Le Callonnec <sebastien@weblogism.com>
2020-06-01 08:14:03 +0100

    Handle font management identically across themes.

--- src/main/java/core/gui/theme/light/SolarizedLightTheme.java
+++ src/main/java/core/gui/theme/light/SolarizedLightTheme.java
@@ -2,16 +2,14 @@ package core.gui.theme.light;
 
 import com.github.weisj.darklaf.DarkLaf;
 import com.github.weisj.darklaf.LafManager;
+import core.gui.theme.BaseTheme;
 import core.gui.theme.HOBooleanName;
-import core.gui.theme.Theme;
 import core.gui.theme.ThemeManager;
 import core.model.UserParameter;
 
 import javax.swing.*;
-import java.util.HashMap;
-import java.util.Map;
 
-public class SolarizedLightTheme implements Theme {
+public class SolarizedLightTheme extends BaseTheme {
     public final static String THEME_NAME = "Solarized Light";
 
     /**
@@ -28,9 +26,7 @@ public class SolarizedLightTheme implements Theme {
             LafManager.setTheme(new com.github.weisj.darklaf.theme.SolarizedLightTheme());
             UIManager.setLookAndFeel(DarkLaf.class.getCanonicalName());
 
-            Map<String, Object> properties = new HashMap<>();
-            properties.put("fontSize", UserParameter.instance().schriftGroesse);
-
+            setFont(UserParameter.instance().schriftGroesse);
             ThemeManager.instance().put(HOBooleanName.IMAGEPANEL_BG_PAINTED, false);
             return true;
         } catch (Exception e) {
