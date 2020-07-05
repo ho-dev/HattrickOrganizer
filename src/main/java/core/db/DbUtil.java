96589d7855412763b1265a73debb36d060050fee
Wolfgang Schneider <wolfgang.schneider@zissener-weg-brenk.de>
2020-07-06 00:33:35 +0200

    #623: DbUtil.getNullableBoolean

--- src/main/java/core/db/DbUtil.java
+++ src/main/java/core/db/DbUtil.java
@@ -5,8 +5,14 @@ import java.sql.SQLException;
 
 public class DbUtil {
 
-    static public Integer getNullableInt(ResultSet rs, String cl) throws SQLException {
+    public static Integer getNullableInt(ResultSet rs, String cl) throws SQLException {
         Integer res = rs.getInt(cl);
+        if (!rs.wasNull()) return  res;
+        return null;
+    }
+
+    public static Boolean getNullableBoolean(ResultSet rs, String cl) throws SQLException {
+        Boolean res = rs.getBoolean(cl);
         if (!rs.wasNull()) return  res;
         return null;
     }
