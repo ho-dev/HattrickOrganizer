1286ad132ddb0474157560de7f1a74ca75f50d31
akasolace <thisiakasolace@gmail.com>
2020-08-19 23:18:20 +0200

    closes #644 continued work on update functionality

--- src/main/java/tool/updater/UpdateController.java
+++ src/main/java/tool/updater/UpdateController.java
@@ -169,24 +169,23 @@ public final class UpdateController {
     public static void updateHO(final String urlString) {
         if (HO.isPortableVersion()) {
             // HO! manage the (partial) update
-        File tmp = new File("update.zip");
-        LoginWaitDialog wait = new LoginWaitDialog(HOMainFrame.instance());
-        wait.setVisible(true);
-        if (!UpdateHelper.download(urlString, tmp)) {
+            File tmp = new File("update.zip");
+            LoginWaitDialog wait = new LoginWaitDialog(HOMainFrame.instance());
+            wait.setVisible(true);
+            if (!UpdateHelper.download(urlString, tmp)) {
+                wait.setVisible(false);
+                HOLogger.instance().error(UpdateController.class, "Could not download: " + urlString);
+                return;
+            }
             wait.setVisible(false);
-            HOLogger.instance().error(UpdateController.class, "Could not download: " + urlString);
-            return;
-        }
-        wait.setVisible(false);
 
-        JOptionPane.showMessageDialog(null,
-                HOVerwaltung.instance().getLanguageString("NeustartErforderlich"), HOVerwaltung.instance()
-                        .getLanguageString("ls.menu.file.update") + " - "+ HOVerwaltung.instance()
-                        .getLanguageString("ls.menu.file.update.ho"),
-                JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null,
+                    HOVerwaltung.instance().getLanguageString("NeustartErforderlich"), HOVerwaltung.instance()
+                            .getLanguageString("ls.menu.file.update") + " - "+ HOVerwaltung.instance()
+                            .getLanguageString("ls.menu.file.update.ho"),
+                    JOptionPane.INFORMATION_MESSAGE);
 
-        HOMainFrame.instance().shutdown();
-
+            HOMainFrame.instance().shutdown();
         }
         else {
             // making update via install4J
