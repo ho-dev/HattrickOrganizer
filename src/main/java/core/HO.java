b1a09d3aaa837373a92db97174bfac36b1547627
akasolace <thisisakasolace@gmail.com>
2020-10-19 20:08:14 +0200

    Bug/727 (#734)
    * fixes 706
    
    * closes #727
    
    Co-authored-by: akasolace <thisiakasolace@gmail.com>

--- src/main/java/core/HO.java
+++ src/main/java/core/HO.java
@@ -17,6 +17,7 @@ import core.util.OSUtils;
 import java.io.File;
 import javax.swing.*;
 import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.Locale;
 
 
@@ -81,6 +82,7 @@ public class HO {
 		Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler());
 
 		if (args != null) {
+			if(Arrays.asList(args).contains("INSTALLED")) {portable_version = false;};
 			String arg;
 			for (String _arg : args) {
 				arg = _arg.trim().toUpperCase();
@@ -89,7 +91,6 @@ public class HO {
 					case "DEBUG" -> HOLogger.instance().setLogLevel(HOLogger.DEBUG);
 					case "WARNING" -> HOLogger.instance().setLogLevel(HOLogger.WARNING);
 					case "ERROR" -> HOLogger.instance().setLogLevel(HOLogger.ERROR);
-					case "INSTALLED" -> portable_version = false;
 				}
 			}
 		}
