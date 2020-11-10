78c4e488c5d25f3d88b022707ae2cbe529afa394
akasolace <thisisakasolace@gmail.com>
2020-11-10 08:44:24 +0100

    Reintroduce rendering of null element in PlayerCBItemRenderer (#778)
    In statistics panel, there could be some null elements in the dropdown list

--- src/main/java/core/gui/model/PlayerCBItemRenderer.java
+++ src/main/java/core/gui/model/PlayerCBItemRenderer.java
@@ -1,4 +1,7 @@
 package core.gui.model;
+
+import core.gui.comp.entry.ColorLabelEntry;
+import core.gui.comp.renderer.HODefaultTableCellRenderer;
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,6 +10,18 @@ public class PlayerCBItemRenderer implements javax.swing.ListCellRenderer<Spiele
 
     @Override
     public Component getListCellRendererComponent(JList<? extends SpielerCBItem> list, SpielerCBItem value, int index, boolean isSelected, boolean cellHasFocus) {
-        return value.getListCellRendererComponent(list, index, isSelected);
+        if (value != null) {
+            return value.getListCellRendererComponent(list, index, isSelected);
+        } else {
+            javax.swing.JLabel m_jlBlank = new javax.swing.JLabel(" ");
+            m_jlBlank.setOpaque(true);
+            if (isSelected) {
+                m_jlBlank.setBackground(HODefaultTableCellRenderer.SELECTION_BG);
+            } else {
+                m_jlBlank.setBackground(ColorLabelEntry.BG_STANDARD);
+            }
+            return m_jlBlank;
+        }
     }
+
 }
