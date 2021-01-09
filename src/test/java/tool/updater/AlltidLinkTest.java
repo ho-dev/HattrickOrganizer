9a6387633b835251b2b9daa417810b3c113c6151
Sébastien Le Callonnec <sebastien@weblogism.com>
2021-01-09 15:43:56 +0000

    Add alltid cert to truststore. (#847)

--- src/test/java/tool/updater/AlltidLinkTest.java
+++ src/test/java/tool/updater/AlltidLinkTest.java
@@ -1,16 +1,29 @@
 package tool.updater;
 
-import com.github.weisj.darklaf.LafManager;
-import core.gui.theme.ImageUtilities;
-import core.model.player.IMatchRoleID;
+import core.db.user.UserManager;
 import module.series.statistics.DataDownloader;
 
-import javax.swing.*;
-import java.awt.*;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.Map;
 
 public class AlltidLinkTest {
+    private static void setEnvVariable(String name, String value) {
+        try {
+            Map<String, String> env = System.getenv();
+            Field field = env.getClass().getDeclaredField("m");
+            field.setAccessible(true);
+            ((Map<String, String>) field.get(env)).put(name, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
+        setEnvVariable("AppData",
+                new File("").getAbsoluteFile().getParentFile().getAbsolutePath());
+
+        UserManager.instance().getCurrentUser();
 
         var res = DataDownloader.instance().fetchLeagueTeamPowerRatings(3193, 10, 76);
         System.out.print(res);
