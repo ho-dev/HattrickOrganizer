a097d0b6e3c6e27cd87c4a570c16f4345c8aa0bd
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2021-02-13 11:52:32 +0100

    Trainingtable upgrade (#907)
    * #902 start migration of TRAINING data
    
    * #902 continue migration of TRAINING data
    
    * #902 db update create training list from hrf
    
    * #902 some comments

--- src/main/java/core/db/DBUpdater.java
+++ src/main/java/core/db/DBUpdater.java
@@ -1,9 +1,11 @@
 package core.db;
 
+import core.model.HOVerwaltung;
 import core.model.enums.DBDataSource;
 import core.module.IModule;
 import core.module.ModuleManager;
 import core.module.config.ModuleConfig;
+import core.training.TrainingWeekManager;
 import core.util.HOLogger;
 import module.playeranalysis.PlayerAnalysisModule;
 
@@ -265,6 +267,7 @@ final class DBUpdater {
 						var coTrainer = rs.getInt("COTRAINER");
 						var trainer = rs.getInt("TRAINER");
 
+						// update new columns
 						String update = "update " + TrainingsTable.TABLENAME +
 								" SET" +
 								" TRAINING_DATE='" +
@@ -295,19 +298,18 @@ final class DBUpdater {
 				trainingTable.tryDeleteColumn("YEAR");
 				trainingTable.tryDeleteColumn("WEEK");
 
+				// Fill training table using information from VEREIN, PLAYER and XTRADATA (Step 2)
+				var sql = "select ACTIVATIONDATE FROM BASICS LIMIT 1";
+				rs = m_clJDBCAdapter.executeQuery(sql);
+				if (rs != null) {
+					rs.next();
+					var activationdate = rs.getTimestamp("ACTIVATIONDATE").toInstant();
+					// TODO all happens on construction. I would prefer a static method for such jobs
+					new TrainingWeekManager(activationdate, false, false);
+				}
 			} catch (Exception e) {
 				HOLogger.instance().log(getClass(),"DatenbankZugriff.getTraining " + e);
 			}
-
-
-
-			// fill up the holes (step 2 of data migration)
-			// var trainingDate = getFirstHrfTrainingDate()
-			// while ( trainingDate.before(today) {
-			//    if ( !training(trainingDate).exists ) {
-			//       WeeklyTraining.createTrainingEntry(trainingDate); // method will fetch training info from VEREIN, XTRADATA,...
-			//    }
-			// }
 		}
 
 
