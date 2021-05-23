f80e13f298e8bd8797d99b44d310384fa8d574e3
Wolfgang Schneider <wolfgang.schneider@zissener-weg-brenk.de>
2021-05-23 18:05:40 +0200

    fix wrong keep max skill values

--- src/main/java/module/youth/YouthSkillsInfo.java
+++ src/main/java/module/youth/YouthSkillsInfo.java
@@ -126,7 +126,8 @@ public class YouthSkillsInfo extends HashMap<Skills.HTSkillID, YouthSkillInfo> {
                     if (skill.isTop3() == null) skill.setIsTop3(true);
                 }
             }
-        } else {
+        } else if (minTop3Max < 5) { // reduce max only if it is lower than 5
+            // (otherwise it could conflict with keeper estimations)
             // three skills are marked as top3 skill
             // mark 4 other skills and set maximum to lowest top3 max
             for (var skill : this.values()) {
