fb0cead7663fbce4c61ff5417542dcc61069d4be
MaSedlacek <53978029+MaSedlacek@users.noreply.github.com>
2021-05-31 12:24:53 +0200

    #1016 update design of about window (#1055)
    * Icons' credits removed.
    
    * CHPP logo with white background used.
    
    * HO link added.

--- src/main/java/core/gui/Credits.java
+++ src/main/java/core/gui/Credits.java
@@ -1,22 +1,15 @@
 package core.gui;
 
+import core.HO;
 import core.gui.comp.HyperLinkLabel;
 import core.gui.theme.HOIconName;
 import core.gui.theme.ThemeManager;
 import core.model.HOVerwaltung;
-
-import java.awt.Component;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
-import core.HO;
 import core.util.BrowserLauncher;
 import core.util.HOLogger;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Credits {
 
@@ -27,6 +20,7 @@ public class Credits {
 		gbc.anchor = GridBagConstraints.NORTHWEST;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.gridwidth = 2;
 		creditsPanel.add(new JLabel("Hattrick Organizer " + HO.getVersionString()), gbc);
 		gbc.gridy = 1;
 		creditsPanel.add(new JLabel(" "), gbc);
@@ -42,85 +36,30 @@ public class Credits {
 
 		int lines = gbc.gridy + 1;
 		gbc.gridy = 0;
-		gbc.gridx = 1;
+		gbc.gridx = 2;
 		gbc.gridheight = lines;
 		gbc.anchor = GridBagConstraints.NORTHEAST;
 		gbc.insets = new Insets(0, 15, 0, 0);
-		creditsPanel.add(new JLabel(ThemeManager.getIcon(HOIconName.CHPP)), gbc);
+		creditsPanel.add(new JLabel(ThemeManager.getIcon(HOIconName.CHPP_WHITE_BG)), gbc);
 
-		String fugueURL = "http://p.yusukekamiyamane.com";
-		JPanel fuguePanel = new JPanel(new GridBagLayout());
+		JPanel hoPanel = new JPanel(new GridBagLayout());
 		gbc = new GridBagConstraints();
-		gbc.anchor = GridBagConstraints.NORTHWEST;
-		fuguePanel.add(new JLabel(HOVerwaltung.instance().getLanguageString("credits.someIconsBy")
-				+ " "), gbc);
-		gbc.gridx = 1;
-		JLabel linkLabel = new HyperLinkLabel("Yusuke Kamiyamane", fugueURL);
-		fuguePanel.add(linkLabel, gbc);
-		gbc.gridx = 2;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.weightx = 1.0;
-		fuguePanel.add(
-				new JLabel(". "
-						+ HOVerwaltung.instance().getLanguageString("credits.allRightsReserved")),
-				gbc);
 
-		String fatcowURL = "http://www.fatcow.com/free-icons";
-		JPanel fatcowPanel = new JPanel(new GridBagLayout());
-		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
 		gbc.anchor = GridBagConstraints.NORTHWEST;
-		fatcowPanel.add(new JLabel(HOVerwaltung.instance().getLanguageString("credits.someIconsBy")
-				+ " "), gbc);
-		gbc.gridx = 1;
-		linkLabel = new HyperLinkLabel("Fatcow Hosting", fatcowURL);
-		fatcowPanel.add(linkLabel, gbc);
-		gbc.gridx = 2;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.weightx = 1.0;
-		fatcowPanel.add(
-				new JLabel(". "
-						+ HOVerwaltung.instance().getLanguageString("credits.allRightsReserved")),
-				gbc);
-		
-		String axialisURL = "http://www.axialis.com/free/icons/";
-		JPanel axialisPanel = new JPanel(new GridBagLayout());
-		gbc = new GridBagConstraints();
-		gbc.anchor = GridBagConstraints.NORTHWEST;
-		axialisPanel.add(new JLabel(HOVerwaltung.instance().getLanguageString("credits.someIconsBy")
-				+ " "), gbc);
-		gbc.gridx = 1;
-		linkLabel = new HyperLinkLabel("Axialis Software", axialisURL);
-		axialisPanel.add(linkLabel, gbc);
-		gbc.gridx = 2;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.weightx = 1.0;
-		axialisPanel.add(
-				new JLabel(". "
-						+ HOVerwaltung.instance().getLanguageString("credits.allRightsReserved")),
-				gbc);
-		
-		String brsevURL = "http://brsev.deviantart.com";
-		JPanel brsevPanel = new JPanel(new GridBagLayout());
-		gbc = new GridBagConstraints();
-		gbc.anchor = GridBagConstraints.NORTHWEST;
-		brsevPanel.add(new JLabel(HOVerwaltung.instance().getLanguageString("credits.someIconsBy")
-				+ " "), gbc);
-		gbc.gridx = 1;
-		linkLabel = new HyperLinkLabel("Brsev", brsevURL);
-		brsevPanel.add(linkLabel, gbc);
-		gbc.gridx = 2;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.weightx = 1.0;
-		brsevPanel.add(
-				new JLabel(". "
-						+ HOVerwaltung.instance().getLanguageString("credits.allRightsReserved")),
-				gbc);
+		JLabel hoLabel = new JLabel("HO website: ");
+		hoPanel.add(hoLabel, gbc);
 
-		Object[] components = { creditsPanel, fuguePanel, fatcowPanel, axialisPanel, brsevPanel };
+		gbc.gridx = 1;
+		String gitlabURL = "https://akasolace.github.io/HO/";
+		JLabel linkLabel = new HyperLinkLabel("https://akasolace.github.io/HO/", gitlabURL);
+		hoPanel.add(linkLabel, gbc);
+
+		creditsPanel.add(hoPanel, gbc);
 
 		Object[] options1 = { HOVerwaltung.instance().getLanguageString("window.about.licence"), HOVerwaltung.instance().getLanguageString("ls.button.ok")};
 
-		int result = JOptionPane.showOptionDialog(parent, components, HOVerwaltung.instance().getLanguageString("window.about.title"), JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null,options1, null);
+		int result = JOptionPane.showOptionDialog(parent, creditsPanel, HOVerwaltung.instance().getLanguageString("window.about.title"), JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null,options1, null);
 		if (result == JOptionPane.YES_OPTION) {
 			try {
 				BrowserLauncher.openURL("https://raw.githubusercontent.com/akasolace/HO/master/LICENSE");
