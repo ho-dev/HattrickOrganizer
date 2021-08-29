0c0f46e6f8d60acb136e6dc9578b79aa2157e5f5
Wolfgang Schneider <wolfgang.schneider@zissener-weg-brenk.de>
2021-08-29 14:27:27 +0200

    fix update of youth player overview

--- src/main/java/module/youth/YouthTrainingView.java
+++ src/main/java/module/youth/YouthTrainingView.java
@@ -73,10 +73,5 @@ public class YouthTrainingView extends JScrollPane implements Refreshable {
     public void storeUserSettings() {
         this.tableModel.storeUserSettings(table);
     }
-/*
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        var youthPanel = (YouthPanel) HOMainFrame.instance().getTabbedPane().getModulePanel(IModule.YOUTH);
-        youthPanel.refreshYouthPlayerView();
-    }*/
+
 }
