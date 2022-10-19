117741637f3355d6e73c7945eaa7009c6e228f98
wsbrenk <zissener-weg-brenk.de>
2022-10-19 19:47:17 +0200

    #1593 TournamentDetailsTable

--- src/main/java/core/db/TournamentDetailsTable.java
+++ src/main/java/core/db/TournamentDetailsTable.java
@@ -2,7 +2,6 @@ package core.db;
 
 import core.model.Tournament.TournamentDetails;
 import core.util.HODateTime;
-import core.util.HOLogger;
 import java.sql.Types;
 
 public final class TournamentDetailsTable extends AbstractTable {
@@ -15,79 +14,33 @@ public final class TournamentDetailsTable extends AbstractTable {
 
 	@Override
 	protected void initColumns() {
-		columns = new ColumnDescriptor[14];
-		columns[0]= new ColumnDescriptor("TournamentId",Types.INTEGER,false,true); //The globally unique identifier of the match
-		columns[1]= new ColumnDescriptor("Name",Types.VARCHAR,false,256);
-		columns[2]= new ColumnDescriptor("TournamentType",Types.INTEGER,false);
-		columns[3]= new ColumnDescriptor("Season",Types.SMALLINT,false);  //Current season
-		columns[4]= new ColumnDescriptor("LogoUrl",Types.VARCHAR,true,256); //The url to the logo
-		columns[5]= new ColumnDescriptor("TrophyType",Types.INTEGER,false);
-		columns[6]= new ColumnDescriptor("NumberOfTeams",Types.INTEGER,false);
-		columns[7]= new ColumnDescriptor("NumberOfGroups",Types.INTEGER,false);
-		columns[8]= new ColumnDescriptor("LastMatchRound",Types.TINYINT,false);
-		columns[9]= new ColumnDescriptor("FirstMatchRoundDate",Types.TIMESTAMP,false);
-		columns[10]= new ColumnDescriptor("NextMatchRoundDate",Types.TIMESTAMP,true); //The date of the next match round. During matches this will be the Next match round.
-		columns[11]= new ColumnDescriptor("IsMatchesOngoing",Types.BOOLEAN,false);
-		columns[12]= new ColumnDescriptor("Creator_UserID",Types.INTEGER,false);
-		columns[13]= new ColumnDescriptor("Creator_Loginname",Types.VARCHAR,true, 256);
+		columns = new ColumnDescriptor[]{
+				ColumnDescriptor.Builder.newInstance().setColumnName("TournamentId").setGetter((p) -> ((TournamentDetails) p).getTournamentId()).setSetter((p, v) -> ((TournamentDetails) p).setTournamentId((int) v)).setType(Types.INTEGER).isNullable(false).isPrimaryKey(true).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("Name").setGetter((p) -> ((TournamentDetails) p).getName()).setSetter((p, v) -> ((TournamentDetails) p).setName((String) v)).setType(Types.VARCHAR).setLength(256).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("TournamentType").setGetter((p) -> ((TournamentDetails) p).getTournamentType()).setSetter((p, v) -> ((TournamentDetails) p).setTournamentType((int) v)).setType(Types.INTEGER).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("Season").setGetter((p) -> ((TournamentDetails) p).getSeason()).setSetter((p, v) -> ((TournamentDetails) p).setSeason((short) (int) v)).setType(Types.SMALLINT).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("LogoUrl").setGetter((p) -> ((TournamentDetails) p).getLogoUrl()).setSetter((p, v) -> ((TournamentDetails) p).setLogoUrl((String) v)).setType(Types.VARCHAR).setLength(256).isNullable(true).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("TrophyType").setGetter((p) -> ((TournamentDetails) p).getTrophyType()).setSetter((p, v) -> ((TournamentDetails) p).setTrophyType((int) v)).setType(Types.INTEGER).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("NumberOfTeams").setGetter((p) -> ((TournamentDetails) p).getNumberOfTeams()).setSetter((p, v) -> ((TournamentDetails) p).setNumberOfTeams((int) v)).setType(Types.INTEGER).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("NumberOfGroups").setGetter((p) -> ((TournamentDetails) p).getNumberOfGroups()).setSetter((p, v) -> ((TournamentDetails) p).setNumberOfGroups((int) v)).setType(Types.INTEGER).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("LastMatchRound").setGetter((p) -> ((TournamentDetails) p).getLastMatchRound()).setSetter((p, v) -> ((TournamentDetails) p).setLastMatchRound((short) (int) v)).setType(Types.TINYINT).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("FirstMatchRoundDate").setGetter((p) -> ((TournamentDetails) p).getFirstMatchRoundDate()).setSetter((p, v) -> ((TournamentDetails) p).setFirstMatchRoundDate((HODateTime) v)).setType(Types.TIMESTAMP).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("NextMatchRoundDate").setGetter((p) -> ((TournamentDetails) p).getNextMatchRoundDate()).setSetter((p, v) -> ((TournamentDetails) p).setNextMatchRoundDate((HODateTime) v)).setType(Types.TIMESTAMP).isNullable(true).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("IsMatchesOngoing").setGetter((p) -> ((TournamentDetails) p).getMatchesOngoing()).setSetter((p, v) -> ((TournamentDetails) p).setMatchesOngoing((boolean) v)).setType(Types.BOOLEAN).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("Creator_UserID").setGetter((p) -> ((TournamentDetails) p).getCreator_UserId()).setSetter((p, v) -> ((TournamentDetails) p).setCreator_UserId((int) v)).setType(Types.INTEGER).isNullable(false).build(),
+				ColumnDescriptor.Builder.newInstance().setColumnName("Creator_Loginname").setGetter((p) -> ((TournamentDetails) p).getCreator_Loginname()).setSetter((p, v) -> ((TournamentDetails) p).setCreator_Loginname((String) v)).setType(Types.VARCHAR).setLength(256).isNullable(true).build()
+		};
 	}
 
 	public TournamentDetails getTournamentDetails(int tournamentId)
 	{
-		TournamentDetails oTournamentDetails = null;
-		try {
-			var rs = executePreparedSelect(tournamentId);
-			assert rs != null;
-			if (rs.next()) {
-				oTournamentDetails = new TournamentDetails();
-				oTournamentDetails.setTournamentId(rs.getInt("TournamentId"));
-				oTournamentDetails.setName(rs.getString("Name"));
-				oTournamentDetails.setTournamentType(rs.getInt("TournamentType"));
-				oTournamentDetails.setSeason((short)rs.getInt("Season"));
-				oTournamentDetails.setLogoUrl(rs.getString("LogoUrl"));
-				oTournamentDetails.setTrophyType(rs.getInt("TrophyType"));
-				oTournamentDetails.setNumberOfTeams(rs.getInt("NumberOfTeams"));
-				oTournamentDetails.setNumberOfGroups(rs.getInt("NumberOfGroups"));
-				oTournamentDetails.setLastMatchRound((short)rs.getInt("LastMatchRound"));
-				oTournamentDetails.setFirstMatchRoundDate(HODateTime.fromDbTimestamp(rs.getTimestamp("FirstMatchRoundDate")));
-				oTournamentDetails.setFirstMatchRoundDate(HODateTime.fromDbTimestamp(rs.getTimestamp("NextMatchRoundDate")));
-				oTournamentDetails.setMatchesOngoing(rs.getBoolean("IsMatchesOngoing"));
-				oTournamentDetails.setCreator_UserId(rs.getInt("Creator_UserID"));
-				oTournamentDetails.setCreator_Loginname(rs.getString("Creator_Loginname"));
-			}
-		}
-		catch (Exception e) {
-			HOLogger.instance().log(getClass(),
-					"DB.getTournamentDetails Error" + e);
-		}
-		return oTournamentDetails;
+		return loadOne(TournamentDetails.class, tournamentId);
 	}
 
 	/**
 	 * Store Tournament Details into DB
 	 */
 	void storeTournamentDetails(TournamentDetails details) {
-		try {
-			executePreparedInsert(
-					details.getTournamentId(),
-					details.getName(),
-					details.getTournamentType(),
-					details.getSeason(),
-					details.getLogoUrl(),
-					details.getTrophyType(),
-					details.getNumberOfTeams(),
-					details.getNumberOfGroups(),
-					details.getLastMatchRound(),
-					HODateTime.toDbTimestamp(details.getFirstMatchRoundDate()),
-					HODateTime.toDbTimestamp(details.getNextMatchRoundDate()),
-					details.getMatchesOngoing(),
-					details.getCreator_UserId(),
-					details.getCreator_Loginname()
-			);
-		} catch (Exception e) {
-			HOLogger.instance().log(getClass(),
-					"DB.storeTournamentDetails Error" + e);
-			HOLogger.instance().log(getClass(), e);
-		}
+		store(details);
 	}
 }
