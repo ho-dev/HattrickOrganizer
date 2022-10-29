6f5623b5ff925b275b6fd072ff50b1ff82b4369a
wsbrenk <zissener-weg-brenk.de>
2022-10-29 17:52:15 +0200

    #1593 AbstractTable some javadocs

--- src/main/java/core/db/AbstractTable.java
+++ src/main/java/core/db/AbstractTable.java
@@ -49,29 +49,50 @@ public abstract class AbstractTable {
 		return "CACHED";
 	}
 
+	/**
+	 * derived table class has to create the colums array
+	 */
 	protected abstract void initColumns();
 
 
+	/**
+	 * return the table name
+	 * @return String
+	 */
 	protected String getTableName() {
 		return tableName;
 	}
 
-	protected void setColumns(ColumnDescriptor[] columns) {
-		this.columns = columns;
-	}
-
+	/**
+	 * return the table columns
+	 * @return array of ColumnDescriptor
+	 */
 	protected ColumnDescriptor[] getColumns() {
 		return columns;
 	}
 
+	/**
+	 * standard creates no index
+	 * @return empty String array of create index statements
+	 */
 	protected String[] getCreateIndexStatement() {
 		return new String[0];
 	}
 
+	/**
+	 * standard creates no constraints. Constrains are added to the create table statements.
+	 * @return empty String array of constraints
+	 */
 	protected String[] getConstraintStatements() {
 		return new String[0];
 	}
 
+	/**
+	 * stores the given object.
+	 * if the object is already stored in database, update is called otherwise insert
+	 * @param object that should be stored
+	 * @param <T> Storable class (extends Abstract.Storable)
+	 */
 	public <T extends Storable> void store(T object){
 		if (object.isStored()) {
 			update(object);
@@ -80,10 +101,24 @@ public abstract class AbstractTable {
 		}
 	}
 
+	/**
+	 * create a new record of the storable object
+	 * @param object that should be created
+	 * @param <T> Storable class (extends Abstract.Starable)
+	 * @return 1 on success, 0 on error
+	 */
 	private <T extends Storable> int insert(T object) {
 		return executePreparedInsert(Arrays.stream(columns).map(c->c.getter.apply(object)).toArray());
 	}
 
+	/**
+	 * update an existing record.
+	 * The first columns of the table are used in the where clause, the remaining as set values.
+	 * Count of id columns is defined by field idcolumns.
+	 * @param object that should be updated
+	 * @param <T> Storable class (extends Abstract.Stobable)
+	 * @return 1 on success, 0 on error
+	 */
 	private <T extends Storable> int update(T object) {
 		var values = new ArrayList<>();
 		values.addAll(Arrays.stream(columns).skip(idColumns).map(c->c.getter.apply(object)).toList());
@@ -91,9 +126,27 @@ public abstract class AbstractTable {
 		return executePreparedUpdate(values.toArray());
 	}
 
+	/**
+	 * load one object.
+	 * The first columns of the table are used as id columns.
+	 * the specified where values must match the first id columns of the table.
+	 * The count of where values is defined by idColumns
+	 * @param tClass Storable class (extends Abstract.Storable)
+	 * @param whereValues variable arguments descibing the where values (count must match idColumns)
+	 * @param <T>
+	 * @return
+	 */
 	public <T extends Storable> T loadOne(Class<T> tClass, Object ... whereValues) {
 		return loadOne(tClass, executePreparedSelect(whereValues));
 	}
+
+	/**
+	 * load one object of an externally created result set.
+	 * @param tClass Storable class (extends Abstract.Storable)
+	 * @param rs result set
+	 * @param <T>
+	 * @return
+	 */
 	public <T extends Storable> T loadOne(Class<T> tClass, ResultSet rs) {
 		var list = load(tClass, rs, 1);
 		if ( list.size() > 0){
@@ -102,13 +155,36 @@ public abstract class AbstractTable {
 		return null;
 	}
 
+	/**
+	 * load a list of records
+	 * @param tClass Storable class (extends Abstract.Storable)
+	 * @param whereValues variable arguments descibing the where values (count must match idColumns)
+	 * @param <T>
+	 * @return List of objects of type T
+	 */
 	public <T extends Storable> List<T> load(Class<T> tClass, Object ... whereValues) {
 		return load(tClass, executePreparedSelect(whereValues), -1);
 	}
+
+	/**
+	 * load a list of records of an externally created result set
+	 * @param tClass Storable class (extends Abstract.Storable)
+	 * @param rs result set
+	 * @param <T>
+	 * @return
+	 */
 	public <T extends Storable> List<T> load(Class<T> tClass, ResultSet rs) {
 		return load(tClass, rs, -1);
 	}
 
+	/**
+	 * load a list of records
+	 * @param tClass Storable class (extends Abstract.Storable)
+	 * @param rs result set
+	 * @param max 1 to load one object, -1 to load all objects
+	 * @param <T>
+	 * @return
+	 */
 	protected <T extends  Storable> List<T> load(Class<T> tClass, ResultSet rs, int max){
 		var ret = new ArrayList<T>();
 		ColumnDescriptor columnDescriptor = null;
@@ -147,6 +223,13 @@ public abstract class AbstractTable {
 		return ret;
 	}
 
+	/**
+	 * return a float from a result set column
+	 * @param rs result set
+	 * @param columnName column name
+	 * @return Float, null if the column was empty (null)
+	 * @throws SQLException
+	 */
 	private Float getFloat(ResultSet rs, String columnName) throws SQLException {
 		var ret = rs.getFloat(columnName);
 		if (rs.wasNull()){
@@ -155,6 +238,13 @@ public abstract class AbstractTable {
 		return ret;
 	}
 
+	/**
+	 * return String from a result set column
+	 * @param rs result set
+	 * @param columnName column name
+	 * @return String, null if column was empty (null)
+	 * @throws SQLException
+	 */
 	private String getString(ResultSet rs, String columnName) throws SQLException {
 		var ret = rs.getString(columnName);
 		if (rs.wasNull()){
@@ -163,6 +253,13 @@ public abstract class AbstractTable {
 		return ret;
 	}
 
+	/**
+	 * return HODateTime from a result set column
+	 * @param rs result set
+	 * @param columnName column name
+	 * @return HODateTime, null if the column was empty (null)
+	 * @throws SQLException
+	 */
 	private HODateTime getHODateTime(ResultSet rs, String columnName) throws SQLException {
 		var ts = rs.getTimestamp(columnName);
 		if (rs.wasNull()){
@@ -171,6 +268,13 @@ public abstract class AbstractTable {
 		return HODateTime.fromDbTimestamp(ts);
 	}
 
+	/**
+	 * return Double from a result set column
+	 * @param rs result set
+	 * @param columnName column name
+	 * @return Double, null if column was empty (null)
+	 * @throws SQLException
+	 */
 	private Double getDouble(ResultSet rs, String columnName) throws SQLException {
 		var ret = rs.getDouble(columnName);
 		if (rs.wasNull()){
@@ -179,6 +283,13 @@ public abstract class AbstractTable {
 		return ret;
 	}
 
+	/**
+	 * return Boolean from result set column
+	 * @param rs result set
+	 * @param columnName column name
+	 * @return Boolean, null if column was empty (null)
+	 * @throws SQLException
+	 */
 	private Boolean getBoolean(ResultSet rs, String columnName) throws SQLException {
 		var ret = rs.getBoolean(columnName);
 		if (rs.wasNull()){
@@ -187,6 +298,13 @@ public abstract class AbstractTable {
 		return ret;
 	}
 
+	/**
+	 * return Integer from result set colum
+	 * @param rs result set
+	 * @param columnName column name
+	 * @return Integer, null if column was empty (null)
+	 * @throws SQLException
+	 */
 	private Integer getInteger(ResultSet rs, String columnName) throws SQLException {
 		var ret = rs.getInt(columnName);
 		if (rs.wasNull()){
@@ -197,15 +315,38 @@ public abstract class AbstractTable {
 
 	// Prepared Statements
 	// Insert
+
+	/**
+	 * Statement builder class used to create the insert statement.
+	 */
 	public static class PreparedInsertStatementBuilder extends DBManager.PreparedStatementBuilder {
+		/**
+		 * constructor creates the insert statement
+		 * @param table table that should create the insert statement
+		 */
 		public PreparedInsertStatementBuilder(AbstractTable table) {
 			super(table.createInsertStatement());
 		}
 	}
+
+	/**
+	 * the standard insert statement builder object
+	 */
 	protected PreparedInsertStatementBuilder preparedInsertStatementBuilder;
+
+	/**
+	 * standard method to create the insert statement builder.
+	 * This method should be overridden if a specialized insert is required by a derived class.
+	 * @return PreparedInsertStatementBuilder
+	 */
 	protected PreparedInsertStatementBuilder createPreparedInsertStatementBuilder(){
 		return new PreparedInsertStatementBuilder(this);
 	}
+
+	/**
+	 * create sql string of the standard insert statement.
+	 * @return sql string of the prepared statement.
+	 */
 	private String createInsertStatement() {
 		return "INSERT INTO " + getTableName() +
 				" (" +
@@ -214,6 +355,12 @@ public abstract class AbstractTable {
 				DBManager.getPlaceholders(columns.length) +
 				")";
 	}
+
+	/**
+	 * execute the prepared insert statement.
+	 * @param values array of column values must match the defined columns of the table
+	 * @return 1 on success, 0 on error
+	 */
 	protected int executePreparedInsert(Object ... values){
 		if ( preparedInsertStatementBuilder==null){
 			preparedInsertStatementBuilder=createPreparedInsertStatementBuilder();
@@ -222,6 +369,14 @@ public abstract class AbstractTable {
 	}
 
 	// Update
+
+
+	/**
+	 * create sql string of the standard update statment.
+	 * the first colums of table are used in the where clause, the remaining columns in the SET part.
+	 * The count of where values if given by the field idcolumns
+	 * @return sql string of the prepared statement
+	 */
 	private String createUpdateStatement() {
 		return "UPDATE " + getTableName() +
 				" SET " +
@@ -229,19 +384,47 @@ public abstract class AbstractTable {
 				createSQLWhere();
 	}
 
+	/**
+	 * Update statement builder class used to create a prepared update statement
+	 */
 	public static class PreparedUpdateStatementBuilder extends DBManager.PreparedStatementBuilder {
+		/**
+		 * constructor with user defined set part
+		 * @param table derived table
+		 * @param set String
+		 */
 		public PreparedUpdateStatementBuilder(AbstractTable table, String set) {
 			super("UPDATE " + table.getTableName() + " " + set);
 		}
+
+		/**
+		 * construct a standard update statement builder
+		 * @param table table
+		 */
 		public PreparedUpdateStatementBuilder(AbstractTable table) {
 			super(table.createUpdateStatement());
 		}
 	}
 
+	/**
+	 * the standard update statement builder object.
+	 */
 	protected PreparedUpdateStatementBuilder preparedUpdateStatementBuilder;
+
+	/**
+	 * create the standard update statement builder.
+	 * This method should be overridden if a specialized update is required by a derived class.
+	 * @return PreparedUpdateStatementBuilder
+	 */
 	protected PreparedUpdateStatementBuilder createPreparedUpdateStatementBuilder(){
 		return new PreparedUpdateStatementBuilder(this);
 	}
+
+	/**
+	 * execute the standard update statement
+	 * @param values set first values must match the where clause value (idcolumns). the remaining columns are used in the SET part
+	 * @return 1 on success, 0 on error, -1 if no update statement builder was defined.
+	 */
 	protected int executePreparedUpdate(Object ... values){
 		if (preparedUpdateStatementBuilder==null){
 			preparedUpdateStatementBuilder=createPreparedUpdateStatementBuilder();
@@ -254,18 +437,49 @@ public abstract class AbstractTable {
 	}
 
 	// Delete
+
+
+	/**
+	 * Delete statement builder class
+	 */
 	public static class PreparedDeleteStatementBuilder extends DBManager.PreparedStatementBuilder {
+		/**
+		 * standard constructor uses the first id columns to create the standard delete statement
+		 * @param table table
+		 */
 		public PreparedDeleteStatementBuilder(AbstractTable table) {
 			super("DELETE FROM " + table.getTableName() +  table.createSQLWhere());
 		}
+
+		/**
+		 * constructor with user defined where clause used to create the delete statement
+		 * @param table derived table
+		 * @param where sql where clause
+		 */
 		public PreparedDeleteStatementBuilder(AbstractTable table, String where) {
 			super("DELETE FROM " + table.getTableName() + " " + where);
 		}
 	}
+
+	/**
+	 * the standard delete statement builder object
+	 */
 	protected  PreparedDeleteStatementBuilder preparedDeleteStatementBuilder;
+
+	/**
+	 * create the standard delete statement builder.
+	 * This method should be overridden if a specialized delete is required by a derived class.
+	 * @return PreparedDeleteStatementBuilder
+	 */
 	protected PreparedDeleteStatementBuilder createPreparedDeleteStatementBuilder(){
 		return new PreparedDeleteStatementBuilder(this);
 	}
+
+	/**
+	 * execute the standard delete statement
+	 * @param whereValues the values must match the where clause value (idcolumns)
+	 * @return 1 on success, 0 on error
+	 */
 	protected int executePreparedDelete(Object ... whereValues) {
 		if ( preparedDeleteStatementBuilder==null){
 			preparedDeleteStatementBuilder=createPreparedDeleteStatementBuilder();
@@ -274,30 +488,67 @@ public abstract class AbstractTable {
 	}
 
 	// Select
+
+	/**
+	 * select statement builder class
+	 */
 	public static class PreparedSelectStatementBuilder extends DBManager.PreparedStatementBuilder {
+		/**
+		 * construct select statement with user defined where clause
+		 * @param table derived table
+		 * @param where String sql where clause
+		 */
 		public PreparedSelectStatementBuilder(AbstractTable table, String where) {
 			super("SELECT * FROM " + table.getTableName() + " " + where);
 		}
+
+		/**
+		 * construct standard select statement using the first idcolumns to build the where clause
+		 * @param table derived table
+		 */
 		public PreparedSelectStatementBuilder(AbstractTable table) {
 			super("SELECT * FROM " + table.getTableName() + table.createSQLWhere());
 		}
 	}
 
+	/**
+	 * create the standard where clause using the first idcolumns of the table
+	 * @return String sql where clause
+	 */
 	private String createSQLWhere() {
 		return " WHERE " + Arrays.stream(this.columns).limit(this.idColumns).map(i->i.getColumnName()+"=?").collect(Collectors.joining(" AND "));
 	}
 
+	/**
+	 * the standard select statement builder object
+	 */
 	protected PreparedSelectStatementBuilder preparedSelectStatementBuilder;
+
+	/**
+	 * create the standard select statement builder
+	 * This method should be overridden if a specialized select is required by a derived class.
+	 * @return PreparedSelectStatementBuilder
+	 */
 	protected PreparedSelectStatementBuilder createPreparedSelectStatementBuilder() {
 		return new PreparedSelectStatementBuilder(this);
 	}
 
+	/**
+	 * create the standard prepared select statement
+	 * @return PreparedStatement
+	 */
 	protected PreparedStatement getPreparedSelectStatement(){
 		if ( preparedSelectStatementBuilder==null){
 			preparedSelectStatementBuilder=createPreparedSelectStatementBuilder();
 		}
 		return preparedSelectStatementBuilder.getStatement();
 	}
+
+	/**
+	 * execute the standard select statement
+	 * @param whereValues where values
+	 * @return result set
+	 */
 	protected ResultSet executePreparedSelect (Object ... whereValues){
 		return adapter.executePreparedQuery(getPreparedSelectStatement(), whereValues);
 	}
@@ -324,6 +575,10 @@ public abstract class AbstractTable {
 		return ret;
 	}
 
+	/**
+	 * Create the table
+	 * @throws SQLException
+	 */
 	public void createTable() throws SQLException {
 		if (!tableExists(getTableName())) {
 			ColumnDescriptor[] columns = getColumns();
