e3953651a962c8ac8e6888f6c12f5aadbac9de35
wsbrenk <zissener-weg-brenk.de>
2022-11-01 22:14:39 +0100

    #1674 fix error in YouthTrainingTable

--- src/main/java/core/db/YouthTrainingTable.java
+++ src/main/java/core/db/YouthTrainingTable.java
@@ -19,8 +19,8 @@ public class YouthTrainingTable extends AbstractTable{
         columns = new ColumnDescriptor[]{
                 ColumnDescriptor.Builder.newInstance().setColumnName("MATCHID").setGetter((p) -> ((YouthTraining) p).getYouthMatchId()).setSetter((p, v) -> ((YouthTraining) p).setYouthMatchId( (int) v)).setType(Types.INTEGER).isNullable(false).isPrimaryKey(true).build(),
                 ColumnDescriptor.Builder.newInstance().setColumnName("MatchTyp").setGetter((p) -> ((YouthTraining) p).getMatchType().getId()).setSetter((p, v) -> ((YouthTraining) p).setYouthMatchType(MatchType.getById((int)v))).setType(Types.INTEGER).isNullable(false).build(),
-                ColumnDescriptor.Builder.newInstance().setColumnName("TRAINING1").setGetter((p) -> ((YouthTraining) p).getTraining(YouthTraining.Priority.Primary).getValue()).setSetter((p, v) -> ((YouthTraining) p).setTraining(YouthTraining.Priority.Primary, YouthTrainingType.valueOf((Integer) v))).setType(Types.INTEGER).isNullable(true).build(),
-                ColumnDescriptor.Builder.newInstance().setColumnName("TRAINING2").setGetter((p) -> ((YouthTraining) p).getTraining(YouthTraining.Priority.Secondary).getValue()).setSetter((p, v) -> ((YouthTraining) p).setTraining(YouthTraining.Priority.Secondary, YouthTrainingType.valueOf((Integer) v))).setType(Types.INTEGER).isNullable(true).build()
+                ColumnDescriptor.Builder.newInstance().setColumnName("TRAINING1").setGetter((p) -> YouthTrainingType.getValue(((YouthTraining) p).getTraining(YouthTraining.Priority.Primary))).setSetter((p, v) -> ((YouthTraining) p).setTraining(YouthTraining.Priority.Primary, YouthTrainingType.valueOf((Integer) v))).setType(Types.INTEGER).isNullable(true).build(),
+                ColumnDescriptor.Builder.newInstance().setColumnName("TRAINING2").setGetter((p) -> YouthTrainingType.getValue(((YouthTraining) p).getTraining(YouthTraining.Priority.Secondary))).setSetter((p, v) -> ((YouthTraining) p).setTraining(YouthTraining.Priority.Secondary, YouthTrainingType.valueOf((Integer) v))).setType(Types.INTEGER).isNullable(true).build()
         };
     }
 
