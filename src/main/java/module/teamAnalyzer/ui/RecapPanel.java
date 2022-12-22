0ffbdd8c106884afb73eec987433cc4df4fe263c
wsbrenk <zissener-weg-brenk.de>
2022-12-22 16:03:20 +0100

    #347 FixedColumnsTable

--- src/main/java/module/teamAnalyzer/ui/RecapPanel.java
+++ src/main/java/module/teamAnalyzer/ui/RecapPanel.java
@@ -1,11 +1,9 @@
 package module.teamAnalyzer.ui;
 
-import core.gui.comp.table.ToolTipHeader;
+import core.gui.comp.table.FixedColumnsTable;
 import core.gui.model.UserColumnController;
 import module.teamAnalyzer.report.TeamReport;
 import module.teamAnalyzer.ui.controller.RecapListSelectionListener;
-import module.transfer.ui.sorter.DefaultTableSorter;
-
 import java.awt.*;
 import java.io.Serial;
 import javax.swing.*;
@@ -18,9 +16,9 @@ public class RecapPanel extends JPanel {
     public static final String VALUE_NA = "---"; //$NON-NLS-1$
 
     //~ Instance fields ----------------------------------------------------------------------------
-    private JTable table;
-    //private UiRecapTableModel tableModel;
-    private RecapListSelectionListener recapListener = null;
+    private FixedColumnsTable table;
+
+    private final RecapListSelectionListener recapListener = null;
 
     private RecapPanelTableModel tableModel;
     /**
@@ -38,31 +36,19 @@ public class RecapPanel extends JPanel {
     private void jbInit() {
         tableModel = UserColumnController.instance().getTeamAnalyzerRecapModell();
         tableModel.showTeamReport(null);
-        DefaultTableSorter tableSorter = new DefaultTableSorter(tableModel);
-
-        table = new JTable(tableSorter);
-        ToolTipHeader header = new ToolTipHeader(table.getColumnModel());
-        header.setToolTipStrings(tableModel.getTooltips());
-        header.setToolTipText("");
-        table.setTableHeader(header);
-        tableSorter.setTableHeader(table.getTableHeader());
-
+        table = new FixedColumnsTable(2, tableModel);
         table.setDefaultRenderer(Object.class, new RecapTableRenderer());
         table.setDefaultRenderer(ImageIcon.class, new RecapTableRenderer());
-        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//        restoreUserSettings();
 
-        restoreUserSettings();
-
-        ListSelectionModel rowSM = table.getSelectionModel();
-        recapListener = new RecapListSelectionListener(tableSorter, tableModel);
-        rowSM.addListSelectionListener(recapListener);
+        table.addListSelectionListener( new RecapListSelectionListener(table.getTableSorter(), tableModel));
         setLayout(new BorderLayout());
 
-        JScrollPane scrollPane = new JScrollPane(table);
-
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-        add(scrollPane);
+//        JScrollPane scrollPane = new JScrollPane(table);
+//
+//        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+//        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        add(table);
     }
 
     public String getSelectedTacticType() {
@@ -75,9 +61,5 @@ public class RecapPanel extends JPanel {
 
     public void storeUserSettings() {
         this.tableModel.storeUserSettings(table);
-    }
-
-    public void restoreUserSettings() {
-        this.tableModel.restoreUserSettings(table);
     }
 }
