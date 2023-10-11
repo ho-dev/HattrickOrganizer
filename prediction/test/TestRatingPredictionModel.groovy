019f6ee86fe5e9aefb1a98f6405f01457237d4c6
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2023-10-11 21:03:46 +0200

    #1782 start groovy api

--- prediction/test/TestRatingPredictionModel.groovy
+++ prediction/test/TestRatingPredictionModel.groovy
@@ -1,9 +1,15 @@
 import module.lineup.*
+import core.rating.RatingPredictionModel
 import core.rating.RatingPredictionModel.*
+import core.model.Team
 
-class TestRatingPredictionModel {
+@groovy.transform.InheritConstructors
+class TestRatingPredictionModel extends RatingPredictionModel {
 
-    double c = 1.
-    @override
-    def calcSectorRating(Lineup lineup, RatingSector s, int minute) { c }
+    TestRatingPredictionModel(Team team){
+        super(team)
+    }
+
+    @Override
+    double calcSectorRating(Lineup lineup, RatingSector s, int minute) { return 1 }
 }
