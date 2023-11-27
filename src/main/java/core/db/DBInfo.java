8582c1f641b385d49a31000bbef18a271dd366f4
Sébastien Le Callonnec <sebastien@weblogism.com>
2023-11-27 08:45:57 +0000

    Add unit tests for `ColumnDescriptor`, prior to migration.

--- src/main/java/core/db/DBInfo.java
+++ src/main/java/core/db/DBInfo.java
@@ -7,69 +7,63 @@ import java.sql.Types;
 import java.util.ArrayList;
 
 /**
- * 
  * @author Thorsten Dietz
- *
  */
-final public  class DBInfo {
-	private DatabaseMetaData databaseMetaData;
-	
-	/**
-	 * return String for java.sql.Types
-	 * @param type
-	 * @return
-	 */
-	public String getTypeName(int type){
-		
-		// in future we have to change some type for some db
-		switch(type){
-		case Types.BOOLEAN: return "BOOLEAN";
-		case Types.BIT: return "BIT";
-		case Types.INTEGER: return "INTEGER";
-		case Types.CHAR: return "CHAR";
-		case Types.DATE: return "DATE";
-		case Types.DECIMAL: return "DECIMAL";
-		case Types.DOUBLE: return "DOUBLE";
-		case Types.FLOAT: return "FLOAT";
-		case Types.LONGVARCHAR: return "LONGVARCHAR";
-		case Types.REAL: return "REAL";
-		case Types.SMALLINT: return "SMALLINT";
-		case Types.TIME: return "TIME";
-		case Types.TIMESTAMP: return "TIMESTAMP";
-		case Types.TINYINT: return "TINYINT";
-		case Types.VARCHAR: return "VARCHAR";
-		default:
-				return "";
-		}
-		
-	}
-	
-	protected DBInfo(DatabaseMetaData databaseMetaData){
-		this.databaseMetaData = databaseMetaData;
-	}
-	
-	/**
-	 * return all TableNames from current Database
-	 * @return Object []
-	 */
-	public Object [] getAllTablesNames(){
-		String[] types = new String[2];
+final public class DBInfo {
+    private DatabaseMetaData databaseMetaData;
+
+    /**
+     * return String for java.sql.Types
+     *
+     * @param type
+     * @return String – String representation of the SQL type
+     */
+    public String getTypeName(int type) {
+
+        // in future we have to change some type for some db
+        return switch (type) {
+            case Types.BOOLEAN -> "BOOLEAN";
+            case Types.BIT -> "BIT";
+            case Types.INTEGER -> "INTEGER";
+            case Types.CHAR -> "CHAR";
+            case Types.DATE -> "DATE";
+            case Types.DECIMAL -> "DECIMAL";
+            case Types.DOUBLE -> "DOUBLE";
+            case Types.FLOAT -> "FLOAT";
+            case Types.LONGVARCHAR -> "LONGVARCHAR";
+            case Types.REAL -> "REAL";
+            case Types.SMALLINT -> "SMALLINT";
+            case Types.TIME -> "TIME";
+            case Types.TIMESTAMP -> "TIMESTAMP";
+            case Types.TINYINT -> "TINYINT";
+            case Types.VARCHAR -> "VARCHAR";
+            default -> "";
+        };
+    }
+
+    DBInfo(DatabaseMetaData databaseMetaData) {
+        this.databaseMetaData = databaseMetaData;
+    }
+
+    /**
+     * return all TableNames from current Database
+     *
+     * @return Object []
+     */
+    public Object[] getAllTablesNames() {
+        String[] types = new String[2];
         types[0] = "TABLES"; //some DB want Tables
         types[1] = "TABLE"; // other Table
         ArrayList<String> tables = new ArrayList<String>();
-        try { 
+        try {
             final ResultSet rs = databaseMetaData.getTables(null, null, "%", types);
-            while(rs.next()){
-            	tables.add(rs.getString("TABLE_NAME"));
+            while (rs.next()) {
+                tables.add(rs.getString("TABLE_NAME"));
             }
             rs.close();
-            
- 
-            //this.listTables();
-        }
-        catch (SQLException ex) {
+        } catch (SQLException ex) {
             System.err.println("database connection: " + ex.getMessage());
         }
         return tables.toArray();
-	}
+    }
 }
