41a8eb16e7be11f8afc79f0c950822f9802f9e27
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2024-03-25 23:54:55 +0100

    #1434 download dialog layout

--- src/main/java/core/net/DownloadFilter.java
+++ src/main/java/core/net/DownloadFilter.java
@@ -1,31 +1,35 @@
 package core.net;
 
+import core.db.DBManager;
 import core.model.HOVerwaltung;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class DownloadFilter extends DefaultMutableTreeNode {
 
-    private static HOVerwaltung hov = HOVerwaltung.instance();
-    private DefaultMutableTreeNode currentMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.currentmatches"));
-    private DefaultMutableTreeNode seriesData = new DefaultMutableTreeNode(hov.getLanguageString("download.seriesdata"));
-    private DefaultMutableTreeNode teamData = new DefaultMutableTreeNode(hov.getLanguageString("download.teamdata"));
-    private DefaultMutableTreeNode officialMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.OfficialMatches"));
-    private DefaultMutableTreeNode integratedMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.IntegratedMatches"));
-    private DefaultMutableTreeNode singleMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.SingleMatches"));
-    private DefaultMutableTreeNode ladderMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.LadderMatches"));
-    private DefaultMutableTreeNode tournamentGroupMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.TournamentGroupMatches"));
-    private DefaultMutableTreeNode tournamentPlayoffMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.TournamentPlayoffMatches"));
-    private DefaultMutableTreeNode divisionBattleMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.DivisionBattleMatches"));
+    private static final HOVerwaltung hov = HOVerwaltung.instance();
+    private final DefaultMutableTreeNode currentMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.currentmatches"));
+    private final DefaultMutableTreeNode currentSeriesData = new DefaultMutableTreeNode(hov.getLanguageString("download.currentseriesdata"));
+    private final DefaultMutableTreeNode previousSeriesData = new DefaultMutableTreeNode(hov.getLanguageString("download.oldseriesdata"));
+    private final DefaultMutableTreeNode teamData = new DefaultMutableTreeNode(hov.getLanguageString("download.teamdata"));
+    private final DefaultMutableTreeNode officialMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.OfficialMatches"));
+    private final DefaultMutableTreeNode singleMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.SingleMatches"));
+    private final DefaultMutableTreeNode ladderMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.LadderMatches"));
+    private final DefaultMutableTreeNode tournamentGroupMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.TournamentGroupMatches"));
+    private final DefaultMutableTreeNode tournamentPlayoffMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.TournamentPlayoffMatches"));
+    private final DefaultMutableTreeNode divisionBattleMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.DivisionBattleMatches"));
 
     public DownloadFilter() {
         super(hov.getLanguageString("download.Filter"));
 
         this.add(currentMatches);
+        DefaultMutableTreeNode seriesData = new DefaultMutableTreeNode(hov.getLanguageString("download.seriesdata"));
         this.add(seriesData);
         this.add(teamData);
 
         currentMatches.add(officialMatches);
+        DefaultMutableTreeNode integratedMatches = new DefaultMutableTreeNode(hov.getLanguageString("download.IntegratedMatches"));
         currentMatches.add(integratedMatches);
 
         integratedMatches.add(singleMatches);
@@ -33,6 +37,27 @@ public class DownloadFilter extends DefaultMutableTreeNode {
         integratedMatches.add(tournamentGroupMatches);
         integratedMatches.add(tournamentPlayoffMatches);
         integratedMatches.add(divisionBattleMatches);
+
+        seriesData.add(currentSeriesData);
+        seriesData.add(previousSeriesData);
+
+        fillOldFixturesList(previousSeriesData);
+    }
+
+    private void fillOldFixturesList(DefaultMutableTreeNode previousSeriesData) {
+        final int currentSeason = hov.getModel().getBasics().getSeason();
+        var activationDate = hov.getModel().getBasics().getActivationDate();
+        var htWeek = activationDate.toLocaleHTWeek();
+        var seasons = DBManager.instance().getAllSpielplaene(false);
+        for (int i = currentSeason; i >= htWeek.season; i--) {
+            int finalI = i;
+            var season = seasons.stream().filter(f -> f.getSaison() == finalI).findFirst();
+            var itemText = new StringBuilder(hov.getLanguageString("Season")).append(" ").append(i);
+            if (season.isPresent()) {
+                itemText.append(" / ").append(season.get().getLigaName());
+            }
+            previousSeriesData.add(new DefaultMutableTreeNode(Pair.of(itemText.toString(), finalI)));
+        }
     }
 
     public DefaultMutableTreeNode getCurrentMatches() {
@@ -43,9 +68,8 @@ public class DownloadFilter extends DefaultMutableTreeNode {
         return this.teamData;
     }
 
-    public DefaultMutableTreeNode getSeriesData() {
-        return this.seriesData;
-    }
+    public DefaultMutableTreeNode getCurrentSeriesData() { return this.currentSeriesData; }
+    public DefaultMutableTreeNode getPreviousSeriesData() { return this.previousSeriesData; }
 
     public DefaultMutableTreeNode getOfficialMatches() {
         return this.officialMatches;
