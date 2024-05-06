45fb19603ae0e0dc9b156a2d89c9b6f0bd20aece
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2024-05-06 20:36:38 +0200

    #2065 truncate match report if string is too long for database column

--- src/main/java/core/db/AbstractTable.java
+++ src/main/java/core/db/AbstractTable.java
@@ -44,6 +44,17 @@ public abstract class AbstractTable {
 		initColumns();
 	}
 
+	/**
+	 * Truncate string to maximum length
+	 * @param s String is truncated if it's length exceeds given limit
+	 * @param maxLength Length limit
+	 * @return Truncated string
+	 */
+	static String TruncateString(String s, int maxLength) {
+		if (s != null && s.length() > maxLength) return s.substring(0, maxLength);
+		return s;
+	}
+
 	protected String getTableType() {
 		return "CACHED";
 	}
