c235a044e26834af4eacddd7c95cd5c842bc3966
Sébastien Le Callonnec <sebastien@weblogism.com>
2024-09-16 20:31:20 +0100

    Convert HOZip to Kotlin, and start wiring into GUI.

---
+++ src/test/java/tool/dbencrypter/DbDecryptManagerTest.kt
@@ -0,0 +1,14 @@
+package tool.dbencrypter
+
+import org.junit.jupiter.api.Assertions.*
+
+class DbDecryptManagerTest {
+
+	val privateKey = "MC4CAQAwBQYDK2VwBCIEIG4t5YLFtf2MoN9GN0RTa+0gVIzeXnUxfr+0bpN54FYE"
+	val publicKey = "MCowBQYDK2VwAyEAXge8ZKacaVdVvws1R/5teB0/Y8rDzr05Sk/uBhDwUhw="
+
+	fun process() {
+		//
+	}
+
+}
