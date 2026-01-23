0e5cd249f42806e9d46cff1d16d49071a4c433fb
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2026-01-24 00:56:38 +0100

    Revert "#2379 convert new class CheckBoxTableEntry to kotlin"
    This reverts commit c440e4ab12d508e28774c0b6c3f01e6badedc3b3.

--- src/main/java/core/gui/comp/entry/CheckBoxTableEntry.kt
+++ src/main/java/core/gui/comp/entry/CheckBoxTableEntry.kt
@@ -1,15 +1,17 @@
-package core.gui.comp.entry
+package core.gui.comp.entry;
 
-import core.gui.comp.renderer.HODefaultTableCellRenderer
-import java.awt.Color
-import javax.swing.JCheckBox
-import javax.swing.JComponent
+import core.gui.comp.renderer.HODefaultTableCellRenderer;
+import org.jetbrains.annotations.NotNull;
 
-open class CheckBoxTableEntry(isEnabled: Boolean, var value: Boolean?, fgStandard: Color?, bgStandard: Color?) :
-    IHOTableCellEntry {
-    private val fgStandard: Color?
-    private val bgStandard: Color?
-    private val checkBox: JCheckBox
+import javax.swing.*;
+import java.awt.*;
+
+public class CheckBoxTableEntry implements IHOTableCellEntry {
+
+    private final Color fgStandard;
+    private final Color bgStandard;
+    private final JCheckBox checkBox;
+    private Boolean value;
 
     /**
      * Create table cell entry for booleans by check box
@@ -18,31 +20,32 @@ open class CheckBoxTableEntry(isEnabled: Boolean, var value: Boolean?, fgStandar
      * @param fgStandard Color
      * @param bgStandard Color
      */
-    init {
-        this.checkBox = JCheckBox()
-        this.checkBox.setSelected(value != null && value == true)
-        this.checkBox.setEnabled(isEnabled)
-        this.fgStandard = fgStandard
-        this.bgStandard = bgStandard
-        createComponent()
+    public CheckBoxTableEntry(boolean isEnabled, Boolean value, Color fgStandard, Color bgStandard) {
+        this.value = value;
+        this.checkBox = new JCheckBox();
+        this.checkBox.setSelected(value != null && value);
+        this.checkBox.setEnabled(isEnabled);
+        this.fgStandard = fgStandard;
+        this.bgStandard = bgStandard;
+        createComponent();
     }
 
     /**
      * Set value is called from HOTableModel
      * @param b boolean
      */
-    open fun setValue(b: Boolean) {
-        this.value = b
-        this.checkBox.setSelected(b)
-        this.updateComponent()
+    public void setValue(boolean b) {
+        this.value = b;
+        this.checkBox.setSelected(b);
+        this.updateComponent();
     }
 
     /**
      * Get value is called from HOTableModel
      * @return Boolean
      */
-    fun getValue(): Boolean {
-        return this.value!!
+    public Boolean getValue() {
+        return this.value;
     }
 
     /**
@@ -50,21 +53,24 @@ open class CheckBoxTableEntry(isEnabled: Boolean, var value: Boolean?, fgStandar
      * @param selected boolean
      * @return JComponent
      */
-    override fun getComponent(selected: Boolean): JComponent {
+    @Override
+    public JComponent getComponent(boolean selected) {
         if (selected) {
-            this.checkBox.setBackground(HODefaultTableCellRenderer.SELECTION_BG)
+            this.checkBox.setBackground(HODefaultTableCellRenderer.SELECTION_BG);
+
         } else {
-            this.checkBox.setBackground(bgStandard)
+            this.checkBox.setBackground(bgStandard);
         }
-        this.checkBox.setForeground(if (selected) HODefaultTableCellRenderer.SELECTION_FG else fgStandard)
-        return this.checkBox
+        this.checkBox.setForeground(selected ? HODefaultTableCellRenderer.SELECTION_FG : fgStandard);
+        return this.checkBox;
     }
 
     /**
      * Reset the checkbox
      */
-    override fun clear() {
-        this.checkBox.setSelected(false)
+    @Override
+    public void clear() {
+        this.checkBox.setSelected(false);
     }
 
     /**
@@ -72,16 +78,18 @@ open class CheckBoxTableEntry(isEnabled: Boolean, var value: Boolean?, fgStandar
      * @param obj the object to be compared.
      * @return int [-1,0,1]
      */
-    override fun compareTo(obj: IHOTableCellEntry): Int {
-        if (obj is CheckBoxTableEntry) {
-            if (this.getValue() === obj.getValue()) {
-                return 0
-            } else if (this.getValue()) {
-                return 1
+    @Override
+    public int compareTo(@NotNull IHOTableCellEntry obj) {
+        if (obj instanceof CheckBoxTableEntry entry) {
+            if (this.getValue() == entry.getValue()){
+                return 0;
+            }
+            else if (this.getValue()){
+                return 1;
             }
         }
         // Not a checkbox
-        return -1
+        return -1;
     }
 
     /**
@@ -89,23 +97,26 @@ open class CheckBoxTableEntry(isEnabled: Boolean, var value: Boolean?, fgStandar
      * @param obj IHOTableCellEntry The other entry
      * @return int
      */
-    override fun compareToThird(obj: IHOTableCellEntry): Int {
-        return this.compareTo(obj)
+    @Override
+    public int compareToThird(IHOTableCellEntry obj) {
+        return this.compareTo(obj);
     }
 
     /**
      * Update the component
      */
-    override fun createComponent() {
-        updateComponent()
+    @Override
+    public void createComponent() {
+        updateComponent();
     }
 
     /**
      * Update the component
      */
-    override fun updateComponent() {
-        this.checkBox.setSelected(this.value!!)
-        this.checkBox.setBackground(bgStandard)
-        this.checkBox.setForeground(fgStandard)
+    @Override
+    public void updateComponent() {
+        this.checkBox.setSelected(this.value);
+        this.checkBox.setBackground(bgStandard);
+        this.checkBox.setForeground(fgStandard);
     }
 }
